c8eef51fcb1af51abf084fee8f7a1eb06dba8aed
meliza <>
2003-06-03 02:12:20 +0000

    moved path stuff to top-level

--- wholecell.m
+++ wholecell.m
@@ -15,7 +15,7 @@ switch action
     
 case 'init'
     
-    fixpath;
+    WholeCell('setpath');
     InitWC;
     DAQControl('init');
     ProtocolControl('init');
@@ -24,22 +24,24 @@ case 'destroy'
     clear wc;
     daqreset;
     disp('Cleaned up WholeCell');
+
+case 'setpath'
+    warning off;
+    w = what(pwd);
+    if ~isempty(strmatch('wholecell.m',w.m))
+        rmpath(pwd);
+        rmpath([pwd filesep 'Utility']);
+        rmpath([pwd filesep 'Analysis']);
+        
+        path(path,pwd);
+        path(path,[pwd filesep 'Utility']);
+        path(path,[pwd filesep 'Analysis']);
+        disp('Set up path for wholecell.');
+    end
+    warning on;
     
 otherwise
     disp([action ' is not supported.']);
 end
 
-function fixpath()
 
-warning off;
-w = what(pwd);
-if ~isempty(strmatch('wholecell.m',w.m))
-    rmpath(pwd);
-    rmpath([pwd filesep 'Utility']);
-    rmpath([pwd filesep 'Analysis']);
-    
-    path(path,pwd);
-    path(path,[pwd filesep 'Utility']);
-    path(path,[pwd filesep 'Analysis']);
-end
-warning on;
