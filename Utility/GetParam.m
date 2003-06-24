bde5082d717e93252c251cc6fd5e01a30cae5772
meliza <>
2003-06-24 19:23:13 +0000

    better error checking, changes in param structure

--- Utility/GetParam.m
+++ Utility/GetParam.m
@@ -1,20 +1,25 @@
 function out = GetParam(module, param)
-% Accesses the contents of a GUI object
-% out = GetUIParam(module, param)
+% Accesses the contents of a non-GUI param
+% out = GetParam(module, param)
 %
 %   module - the module in wc
 %   param - the tag for the GUI object
+%   out - the param structure (described in OpenParamFigure.m)
 %
 %   $Id$
 global wc
 
-%param = lower(param);
+param = lower(param);
 module = lower(module);
 
 % find out if the object exists
-sfp = sprintf('isfield(wc.%s,''%s'')',module,param);
+if ~isfield(wc, module)
+    disp(['no such module ' module]);
+    return;
+end
+sfp = sprintf('isfield(wc.%s.param,''%s'')',module,param);
 if (eval(sfp))
-    sf = sprintf('wc.%s.%s', module, param);
+    sf = sprintf('wc.%s.param.%s', module, param);
     out = eval(sf);
 else
     out = [];
