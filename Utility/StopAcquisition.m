02a8e5a1f2050b51c0646a03b313e1857ff08112
meliza <>
2003-06-25 23:12:03 +0000

    moved some cute code into here, not used

--- Utility/StopAcquisition.m
+++ Utility/StopAcquisition.m
@@ -15,3 +15,14 @@ if (isvalid(wc.ai))
     wc.control.protocol = [];
     SetUIParam('wholecell','status','String',get(wc.ai,'Running'));
 end
+%     n = get(wc.ao,'SamplesAvailable');
+%     if n > 0
+%         nt = n;
+%         h = waitbar(0,'Flushing AO...');
+%         while n > 0
+%             n = get(wc.ao,'SamplesAvailable');
+%             waitbar((nt-n)/nt,h);
+%             pause(1);
+%         end
+%         close(h);
+%     end
