2d8adf6bba4a7b908e1fe6fa2a50f8f908ecda42
meliza <>
2003-07-01 22:00:25 +0000

    major overhaul of Episode. Some jitter, but this may be specific to this computer (blue)

--- Episode.m
+++ Episode.m
@@ -18,6 +18,11 @@ function varargout = Episode(varargin)
 
 global wc
 
+if isobject(varargin{1})
+    feval(varargin{3},varargin{1:2});
+    return;
+end
+
 if nargin > 0
 	action = lower(varargin{1});
 else
@@ -27,111 +32,49 @@ end
 switch action
 
 case {'init','reinit'}
-    fig = OpenGuideFigure(me);
+    p = defaultParams;
+    fig = OpenParamFigure(me, p);
     Scope('init');
-    SetUIParam(me,'input_channel','String',GetChannelList(wc.ai));
-    cs = GetChannelList(wc.ao);
-    SetUIParam(me,'stim_channel','String',cs);
-    SetUIParam(me,'command_channel','String',cs);
+    
     EpisodeStats('init','min','','PSR_IR');
     
-    if (isfield(wc.control,'episode'))
-        setValues(wc.control.episode);
-    else
-        setValues; % sets default values based on whole-cell attributes
-    end
-    
 case 'start'
-    control = getValues;
-    setupHardware(control);
-    setupScope(getScope, wc.control.amplifier, control);
-    SetUIParam('wholecell','status','String',get(wc.ai,'Running'));
-    %SetUIParam('scope','status','String',['Sweeps Acquired: 0']);
-    wc.episode.lastlogfilename = get(wc.ai,'LogFileName');
-    [dir newdir] = fileparts(wc.episode.lastlogfilename);
+    setupHardware;
+    setupScope;
+    llf = get(wc.ai,'LogFileName');
+    [dir newdir] = fileparts(llf);
     set(wc.ai,'LogFileName',fullfile(dir, '0000.daq'));
     EpisodeStats('clear');
     startSweep;
+    SetUIParam('wholecell','status','String',get(wc.ai,'Running'));
     
 case 'record'
     switch get(wc.ai,'Running')
     case 'On'
         Episode('stop');
     end
-    control = getValues;
-    setupHardware(control);
-    setupScope(getScope, wc.control.amplifier, control);
-    SetUIParam('wholecell','status','String',get(wc.ai,'Running'));
-    %SetUIParam('scope','status','String',['Sweeps Acquired: 0']);
-    % data is stored in a directory, one file per sweep.
-    wc.episode.lastlogfilename = get(wc.ai,'LogFileName');
-    [dir newdir] = fileparts(wc.episode.lastlogfilename);
+    setupHardware;
+    setupScope;
+
+    llf = get(wc.ai,'LogFileName');
+    [dir newdir] = fileparts(llf);
     s = mkdir(dir, newdir);
     set(wc.ai,'LogFileName',fullfile(dir,newdir, '0000.daq'));
     set(wc.ai,{'LoggingMode','LogToDiskMode'}, {'Disk&Memory','Overwrite'});
     EpisodeStats('clear');
     startSweep;
-    
+    SetUIParam('wholecell','status','String',get(wc.ai,'Running'));    
     
 case 'stop'
-    if (isvalid(wc.ao))
-        set(wc.ao,'StopAction','');
-        stop(wc.ao);
-    end
+    ClearAO(wc.ao);
     if (isvalid(wc.ai))
         stop(wc.ai);
+        set(wc.ai,'SamplesAcquiredAction',{});
         set(wc.ai,'LoggingMode','Memory');
         SetUIParam('wholecell','status','String',get(wc.ai,'Running'));
-        wc.episode.lastlogfilename = [];
         set(wc.ai,'LogFileName',NextDataFile);
     end
 
-case 'sweep'
-    data = varargin{2};
-    time = varargin{3};
-    abstime = varargin{4};
-    if isvalid(wc.ai)
-        fn = get(wc.ai,'LogFileName');
-        set(wc.ai,'LogFileName',NextDataFile(fn));
-    end
-    plotData(data, time, abstime, getScope, wc.control.amplifier.Index);
-    
-case 'newsweep'
-    if ~isempty(wc.episode.lastlogfilename)
-        startSweep;
-    end
-
-case 'value_changed_callback' % called whenever a field is edited
-    wc.control.episode = getValues;
-    
-case 'load_command_callback'
-    [fn pn] = uigetfile('*.mat','Load command pulse');
-    c = load([pn fn]);
-    if (isfield(c,'command'))
-        SetUIParam(me,'command_gain','UserData',c.command);
-        Episode('value_changed_callback');
-    end
-    
-case 'view_command_callback'
-    a = axes;
-    d = GetUIParam(me,'command_gain','UserData');
-    if ~isempty(d)
-        plot(d,'Parent',a);
-    end
-
-case 'load_protocol_callback'
-    [fn pn] = uigetfile('*.mat','Load episode control data...');
-    c = load([pn fn]);
-    if (isfield(c,'control'))
-        wc.control.episode = c.control;
-        setValues(c.control);
-    end
-    
-case 'save_protocol_callback'
-    [fn pn] = uiputfile('*.mat','Save episode control data...');
-    control = getValues;
-    save([pn fn],'control');
-    
 case 'close_callback'
     delete(gcbf);
     
@@ -143,112 +86,82 @@ end
 function out = me()
 out = mfilename;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
-function scope = getScope()
-scope = GetUIHandle('scope','scope');
-if (isempty(scope) | ~ishandle(scope))
-    Scope('init');
-    scope = GetUIHandle('scope','scope');
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function varargout = setValues(varargin)
-% sets values in the GUI to those in a control structure
-% or if no arguments are given, to default values
-global wc
-if (nargin > 0)
-    control = varargin{1};
-    SetUIParam(me,'length','String',num2str(control.length));
-    SetUIParam(me,'frequency','String',num2str(control.frequency));
-    SetUIParam(me,'stim_delay','String',num2str(control.stim_delay));
-    SetUIParam(me,'command_gain','String',num2str(control.command_gain));
-    SetUIParam(me,'command_gain','UserData',control.command);
-    SetUIParam(me,'command_delay','String',num2str(control.command_delay));
-    if (control.input_channel <= length(GetUIParam(me,'input_channel','String')))
-        SetUIParam(me,'input_channel','Value',control.input_channel);
-    end
-    if (control.stim_channel <= length(GetUIParam(me,'stim_channel','String')))
-        SetUIParam(me,'stim_channel','Value',control.stim_channel);
-    end
-    if (control.command_channel <= length(GetUIParam(me,'command_channel','String')))
-        SetUIParam(me,'command_channel','Value',control.command_channel);
-    end
-    
-else
-    SetUIParam(me,'length','String','1000');
-    SetUIParam(me,'frequency','String','0.2');
-    SetUIParam(me,'stim_delay','String','0');
-    SetUIParam(me,'command_gain','String','1');
-    SetUIParam(me,'command_delay','String','0');
-    ic = get(wc.control.amplifier,'Index');
-    if (ic <= length(GetUIParam(me,'input_channel','String')))
-        SetUIParam(me,'input_channel','Value',ic);
-    end
-end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function control = getValues()
-% returns all the values from the gui
-control.length = str2num(GetUIParam(me,'length','String'));
-control.frequency = str2num(GetUIParam(me,'frequency','String'));
-control.stim_delay = str2num(GetUIParam(me,'stim_delay','String'));
-control.command_gain = str2num(GetUIParam(me,'command_gain','String'));
-control.command = GetUIParam(me,'command_gain','UserData');
-control.command_delay = str2num(GetUIParam(me,'command_delay','String'));
-control.input_channel = GetUIParam(me,'input_channel','Value');
-control.stim_channel = GetUIParam(me,'stim_channel','Value');
-control.command_channel = GetUIParam(me,'command_channel','Value');
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%55555
-function varargout = startSweep()
-% Begins a sweep
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function p = defaultParams;
 global wc;
-    stop([wc.ai wc.ao]);
-    putdata(wc.ao, wc.control.pulse);
-    SetUIParam('scope','status','String',get(wc.ai,'logfilename'));
-    start([wc.ai wc.ao]);
-    trigger([wc.ai wc.ao]);
+f = {'description','fieldtype','value','units'};
+f_s = {'description','fieldtype','value'};
+f_l = {'description','fieldtype','value','choices'};
 
+p.inj_length = cell2struct({'Inj Length','value',6,'ms'},f,2);
+p.inj_delay = cell2struct({'Inj Delay','value',200,'ms'},f,2);
+p.inj_gain = cell2struct({'Inj Gain','value',1},f_s,2);
+p.inj_channel = cell2struct({'Command','list',1,GetChannelList(wc.ao)},f_l,2);
+p.stim_delay = cell2struct({'Stim Delay','value',0,'ms'},f,2);
+p.frequency = cell2struct({'Ep. Freq','value',0.2,'Hz'},f,2);
+p.ep_length = cell2struct({'Ep. Length','value',1000,'ms'},f,2);
+p.stim_channel = cell2struct({'Stimulator','list',1,GetChannelList(wc.ao)},f_l,2);
+p.input_channel = cell2struct({'Input','list',1,GetChannelList(wc.ai)},f_l,2);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function varargout = setupHardware(control)
+function setupHardware()
 % Sets up the hardware for this mode of acquisition
 global wc
+display = @updateDisplay;
 
-sr = get(wc.ao, 'SampleRate');
-len = (control.length / 1000) * sr;
-pulse_len = 0.3 * len;
-set(wc.ai,'SamplesPerTrigger',len);
-set(wc.ai,'SamplesAcquiredActionCount',len);
-set(wc.ai,'SamplesAcquiredAction',{'SweepAcquired',me}) % calls SweepAcquired m-file, which deals with data
-set(wc.ao,'StopAction',{'PausedCallback',me,(1/control.frequency - control.length/1000),'newsweep',}); % the daq's timer is used to initiate repeat sweeps
+sr = get(wc.ai, 'SampleRate');
+length = GetParam(me,'ep_length','value');
+len = length * sr / 1000;
+set(wc.ai,'SamplesPerTrigger',len)
+set(wc.ai,'SamplesAcquiredActionCount',len)
+set(wc.ai,'SamplesAcquiredAction',{me, display}) 
+set(wc.ao,'SampleRate',sr)
+set([wc.ai wc.ao],'TriggerType','Manual');
+set(wc.ai,'ManualTriggerHwOn','Trigger');
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
+function queueStimulus()
 % populates the wc.ao data channels
-numouts = length(wc.ao.Channel);
-wc.control.pulse = zeros(len,numouts);
-p = control.stim_delay+1:control.stim_delay+pulse_len;
-wc.control.pulse(p, control.stim_channel) = 10;
-if (~isempty(control.command_gain) & length(control.command) >= len)
-    wc.control.pulse(:,control.command_channel) = control.command(1:len) * control.command_gain;
+global wc
+
+len = get(wc.ai,'SamplesPerTrigger');
+dt = 1000 / get(wc.ai,'SampleRate');
+p = zeros(len, length(wc.ao.Channel));
+% stimulator
+ch = GetParam(me,'stim_channel','value');
+del = GetParam(me,'stim_delay','value') / dt;
+i = del+1:del+0.3*len;
+p(i,ch) = 10;
+% injection
+ch = GetParam(me,'inj_channel','value');
+del = GetParam(me,'inj_delay','value') / dt;
+dur = GetParam(me,'inj_length','value') / dt;
+gain = GetParam(me,'inj_gain','value');
+i = del+1:del+dur;
+p(i,ch) = gain;
+putdata(wc.ao,p);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
+function updateDisplay(obj, event)
+% plots and analyzes the data
+[data, time, abstime] = getdata(obj);
+plotData(data, time, abstime);
+t = 1 / GetParam(me,'frequency','value');
+t2 = GetParam(me,'ep_length','value') / 1000;
+pause(t - t2)
+l = get(obj,'SamplesAcquiredAction');
+if ~isempty(l)
+    startSweep;
 end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function varargout = setupScope(scope, amp, control);
-% sets up the scope properties
-clearPlot(scope);
-%set(scope, 'YLim', [-3 3]);
-set(scope, 'XLim', [0 1000]);
-set(scope, 'NextPlot', 'replacechildren');
-lbl = get(scope,'XLabel');
-set(lbl,'String','Time (ms)');
-lbl = get(scope,'YLabel');
-set(lbl,'String',[get(amp, 'ChannelName') ' (' get(amp,'Units') ')']);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function varargout = plotData(data, time, abstime, scope, index)
+function plotData(data, time, abstime)
 % plots the data
 
+scope = getScope;
 mode = GetParam('control.telegraph', 'mode');
 gain = GetParam('control.telegraph', 'gain');
+index = GetParam(me,'input_channel','value');
 if ~isempty(mode)
     units = TelegraphReader('units',mean(data(:,mode)));
 else
@@ -263,14 +176,45 @@ lbl = get(scope,'YLabel');
 set(lbl,'String',['amplifier (' units ')']);
 % plot the data and average response
 data = AutoGain(data(:,index), gain, units);
-avgdata = get(scope,'UserData');
+avgdata = get(scope, 'UserData');
 avgdata = cat(2, avgdata, data); % TODO: catch irregular sized datas
 Scope('plot','plot',time * 1000, [data mean(avgdata,2)]);
 set(scope,'UserData', avgdata);
 EpisodeStats('plot', abstime, data);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function setupScope();
+% sets up the scope properties
+scope = getScope;
+clearPlot(scope);
+set(scope, 'XLim', [0 1000]);
+set(scope, 'NextPlot', 'replacechildren');
+lbl = get(scope,'XLabel');
+set(lbl,'String','Time (ms)');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%55555
+function startSweep()
+% Begins a sweep
+global wc;
+stop([wc.ai wc.ao]);
+flushdata(wc.ai);
+fn = get(wc.ai,'LogFileName');
+set(wc.ai,'LogFileName',NextDataFile(fn));    
+SetUIParam('scope','status','String',get(wc.ai,'logfilename'));
+queueStimulus;
+start([wc.ai wc.ao]);
+trigger([wc.ai wc.ao]);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
 function clearPlot(axes)
 kids = get(axes, 'Children'); 
 delete(kids);
 set(axes,'UserData',[]);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
+function scope = getScope()
+scope = GetUIHandle('scope','scope');
+if (isempty(scope) | ~ishandle(scope))
+    Scope('init');
+    scope = GetUIHandle('scope','scope');
+end
