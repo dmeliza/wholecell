47b14e071fb1b39c87263cdc18db0f893cd3dae6
meliza <>
2003-08-15 19:59:31 +0000

    working mfile to re-bin multiple datasets

--- Analysis/CombineData.m
+++ Analysis/CombineData.m
@@ -1,6 +1,7 @@
-function [data, time, err] = CombineData(d, t, binsize)
+function [data, time, err, count, Y] = CombineData(d, t, binsize, mode)
 %
-% [d, t, stdev] = CombineData(data, time, binsize)
+% [d, t, stdev, count] = CombineData(data, time, binsize)
+% [output] = CombineData(data,time,binsize,'string') - generates a string array
 % data - 1xM cell array containing 1xQ vectors (Q can vary)
 % time - 1xM cell array containing 1xQ vectors (Q must correspond to data)
 % binsize - size of final bins; should be negative if times are negative
@@ -33,29 +34,36 @@ for j = 1:len
     maxt(j) = max(abs(t{j}));
 end
 maxt = max(maxt);
+Y = sparse(ceil(maxt/bs),len);
 %% iterate through the data
 while i <= maxt
-    X{n,1} = i;
-    X{n,2} = [];
+    [X{n,1}] = deal(i);
+    [X{n,2}] = deal([]);
     for j = 1:len
         tt = abs(t{j});
         ind = find(tt >= i & tt < (i+bs));
-        X{n,2} = cat(2,X{n,2},d{j}(ind));
+        dat = d{j}(ind);
+        X{n,2} = cat(2,X{n,2},dat);
+        if ~isempty(dat)
+            Y(n,j) = mean(dat);
+        end
     end
     i = i + bs;
     n = n+1;
 end
 %% iterate again to generate the final datasets
-len = size(X,1);
-[time,data,err] = deal([]);
-for n = 1:len
-    len = length(time);
-    if ~isempty(X{n,2})
-        time(len+1) = X{n,1};
-        data(len+1) = mean(X{n,2});
-        err(len+1) = std(X{n,2});
+if nargin == 3
+    len = size(X,1);
+    [time,data,err,count] = deal([]);
+    for n = 1:len
+        len = length(time);
+        if ~isempty(X{n,2})
+            time(len+1) = X{n,1};
+            data(len+1) = mean(X{n,2});
+            err(len+1) = std(X{n,2});
+            count(len+1) = length(X{n,2});
+        end
     end
+    time = time * sign(binsize);
 end
-if (binsize < 0)
-    time = -time;
-end
+
