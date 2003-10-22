a74fedfcb945ff1bce21cfc3a6787054979c26ec
lab <>
2003-10-22 00:09:32 +0000

    documentation updated

--- Utility/WriteStructure.m
+++ Utility/WriteStructure.m
@@ -3,6 +3,9 @@ function WriteStructure(filename, structure)
 % are stored individually in the file, so that the command
 % str = load(filename) will reconstruct the structure
 %
+% Usage:
+% WriteStructure(filename, structure)
+%
 % $Id$
 error(nargchk(2,2,nargin));
 if ~isa(structure,'struct')
