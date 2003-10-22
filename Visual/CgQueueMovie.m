31a7ac11e9afa94e1e60cbcd183c5ae1e4cbaebf
lab <>
2003-10-22 00:10:38 +0000

    need to remap the stimulus so that cgloadarray will work

--- Visual/CgQueueMovie.m
+++ Visual/CgQueueMovie.m
@@ -4,11 +4,11 @@ function CgQueueMovie(movie, a_frames)
 % 8-bit movies are supported.  Assumes that the display has already been initialized
 % to 8-bit mode, so that subsequent calls to, e.g., cgloadarray, will work correctly.
 %
-% CgQueueMovie(movie, a_frames)
+% CgQueueMovie(movie, [a_frames])
 %
 % movie - the movie structure, which must have x_res, y_res, colmap, and stimulus
 %         fields defined (see LoadMovie for details on this structure)
-% a_frames - the number of frames to load.
+% a_frames - the number of frames to load (default = size(movie.stimulus,3))
 %
 % $Id$
 
@@ -17,9 +17,20 @@ cgcoltab(0,movie.colmap);
 cgnewpal;
 % load sprites:
 pix = movie.x_res * movie.y_res;
+s = size(movie.stimulus,3);
+if nargin < 2 | s < a_frames
+    a_frames = s;
+end
 h = waitbar(0,['Loading movie (' num2str(a_frames) ' frames)']);
 for i = 1:a_frames
-    cgloadarray(i,x_res,y_res,movie.stimulus(:,:,i),colmap,0);
+    s = cgremap(movie.stimulus(:,:,i));
+    cgloadarray(i,movie.x_res,movie.y_res,s,movie.colmap,0);
     waitbar(i/a_frames,h);
 end
 close(h);
+
+function s = cgremap(s)
+% remaps an array into a vector, which is necessary in order
+% to pass that array to cgloadarray.
+p = prod(size(s));
+s = reshape(s',1,p);
