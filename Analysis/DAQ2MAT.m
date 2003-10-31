7ca64df41b20b6197dcf6c10d0b96384d18f9c2d
meliza <>
2003-10-31 18:00:02 +0000

    moved some functionality into getdaqheader, removed file writing, and fixed indiv mode

--- Analysis/DAQ2MAT.m
+++ Analysis/DAQ2MAT.m
@@ -4,11 +4,11 @@ function [out] = daq2mat(runmode, traceindices)
 % reads in all the daq files in a directory, sorts them by creation time, and
 % outputs a mat file containing the following variables:
 
-% mode: 'stack': arranges traces vertically
+% mode: 'stack': arranges traces vertically. short episodes are discarded
 % data - MxNxP array of traces; traces arranged columnwise; P > 1 if multiple traces are kept
 % time - Mx1 array of times corresponding to rows in data
 % abstime = 1XN array of time offsets corresponding to the start of each trace (sec)
-% info - a structure array of interesting property values (not implemented yet)
+% info - a structure array of interesting property values
 %
 
 % mode: 'cat': concatenates traces horizontally
@@ -18,7 +18,10 @@ function [out] = daq2mat(runmode, traceindices)
 % info - nice info about acquisition
 % data are sorted by relative abstime
 
-% mode: 'indiv': applies gain and mode settings to individual traces
+% mode: 'indiv': applies gain and mode settings to individual traces, generating a
+%                structure array as an output
+%
+% 1.3: NOTE: DAQ2MAT NO LONGER WRITES ANYTHING TO THE DISK
 
 % $Id$
 
@@ -29,32 +32,14 @@ if length(d) == 0
     error('No .daq files in current directory.');
 end
 names = {d.name};
+
 % load info from the first file and figure out what to do with subsequent files
-d = daqread(names{1},'info');
-c = d.ObjInfo.Channel;
-if length(c) > 1
-    cnames = {c.ChannelName};
-    amp = c(strmatch('amplifier',cnames));
-    mode = c(strmatch('mode',cnames));
-    gain = c(strmatch('gain',cnames));
-else
-    amp = c;
-    mode = [];
-    gain = [];
-end
-info.t_unit = 's';
-info.t_rate = d.ObjInfo.SampleRate;
-info.start_time = d.ObjInfo.InitialTriggerTime;
-info.samples = d.ObjInfo.SamplesAcquired;
-if ~isempty(gain)
-    gain = gain.Index;
-end
-if ~isempty(mode)
-    mode = mode.Index;
-end
+info = getdaqheader(names{1});
 if nargin < 2
-    traceindices = amp.Index;
+    traceindices = info.amp;
 end
+amp = info.channels(info.amp);
+
 
 switch lower(runmode)
 case 'stack'
@@ -66,12 +51,11 @@ case 'stack'
         fn = names{i};
         if (exist(fn) > 0)
             [dat, t, at] = daqread(fn);
-            if ~isempty(amp)
-                j = amp.Index;
-                [dat(:,j), units] = ReadDAQScaled(dat, j, gain, mode, amp.Units);
+            if ~isempty(info.gain)
+                j = info.amp;
+                [dat(:,j), units] = ReadDAQScaled(dat, j, info.gain, info.mode, amp.Units);
             end
-            s = sprintf('%s: %d x %d (%s)',fn, length(t), length(traceindices), units);
-            disp(s);
+            fprintf('%s: %d x %d (%s)\n',fn, length(t), length(traceindices), units);
             if length(t) < length(time) | length(t) < 100
                 disp('Data file too short');
             else
@@ -81,16 +65,11 @@ case 'stack'
             end
         end
     end
-    [abstime, ind] = reltimes(abstime);
+    [out.abstime, ind] = reltimes(abstime);
     info.y_unit = units;
-    data = data(:,ind,:);
-    time = single(time);
-    save('daqdata.mat','data','time','abstime','info');
-    out.data = data;
-    out.time = time;
-    out.abstime = abstime;
+    out.data = data(:,ind,:);
+    out.time = single(time);
     out.info = info;
-    disp('Wrote data to daqdata.mat');
 
 case 'cat'
     data = cell(1,length(names));
@@ -100,12 +79,11 @@ case 'cat'
         fn = names{i};
         if (exist(fn) > 0)
             [dat, t, at] = daqread(fn);
-            if ~isempty(amp)
-                j = amp.Index;
-                [dat(:,j), units] = ReadDAQScaled(dat, j, gain, mode, amp.Units);
+            if ~isempty(info.gain)
+                j = info.amp;
+                [dat(:,j), units] = ReadDAQScaled(dat, j, info.gain, info.mode, amp.Units);
             end
-            s = sprintf('%s: %d x %d (%s)',fn, length(t), length(traceindices), units);
-            disp(s);
+            fprintf('%s: %d x %d (%s)\n',fn, length(t), length(traceindices), units);
             time{i} = t;
             data{i} = single(dat(:,traceindices));
             abstime(i,:) = at;
@@ -114,7 +92,7 @@ case 'cat'
     clocks = datenum(abstime);
     [at, ind] = sort(clocks);
     info.y_unit = units;
-    data = cat(1,data{ind});
+    out.data = cat(1,data{ind});
     disp('Aligning times');
     t = [];
     step = 0;
@@ -123,32 +101,26 @@ case 'cat'
         t = cat(1,t,tm + step);
         step = t(end) + t(2) - t(1);
     end
-    time = single(t);
+    out.time = single(t);
     abstime = abstime(1,:);
-    save('daqdata-cat.mat','data','time','abstime','info');
-    out.data = data;
-    out.time = time;
     out.abstime = abstime;
     out.info = info;
-    disp('Wrote data to daqdata-cat.mat');
     
 case 'indiv'
     for i = 1:length(names);
         fn = names{i};
         if (exist(fn) > 0)
             [dat, t, at] = daqread(fn);
-            if ~isempty(amp)
-                j = amp.Index;
-                [dat(:,j), units] = ReadDAQScaled(dat, j, gain, mode, amp.Units);
+            if ~isempty(info.gain)
+                j = info.amp;
+                [dat(:,j), units] = ReadDAQScaled(dat, j, info.gain, info.mode, amp.Units);
             end
-            s = sprintf('%s: %d x %d (%s)',fn, l(i), length(traceindices), units);
-            disp(s);
+            fprintf('%s: %d x %d (%s)\n',fn, length(t), length(traceindices), units);
             info.y_unit = units;
-            data = single(dat(:,tracindices));
-            time = single(t);
-            abstime = at;
-            [pn bn ext] = fileparts(fn);
-            save([bn '.mat'],'data','time','abstime','info');
+            out(i).data = single(dat(:,traceindices));
+            out(i).time = single(t);
+            out(i).abstime = at;
+            out(i).info = info;
         end
     end
     
@@ -161,6 +133,11 @@ function [abstime, ind] = reltimes(abstime)
 % fixes the abstime vector to relative timings
 clocks = datenum(abstime);
 [at, ind] = sort(clocks);
+% remove failed files by clipping out zeros
+i = find(at);
+at = at(i);
+ind = ind(i);
+% convert to relative times (minutes)
 info.starttime = at(1);
 atv = datevec(at - at(1));
 abstime = atv(:,4)*60 + atv(:,5) + atv(:,6)/60;
---
+++ Analysis/GetDAQHeader.m
@@ -0,0 +1,35 @@
+function info = GetDAQHeader(filename)
+% Retrieves header information from a daq file.
+%
+% Usage: info = GetDAQHeader(filename)
+%
+% filename      -   The input file name
+% 
+% info          -   The output structure, which contains the following fields:
+%
+%                   .t_unit     -   the time unit
+%                   .t_rate     -   sampling rate
+%                   .start_time -   the start time (clock vector)
+%                   .samples    -   the number of samples
+%                   .channels   -   structure array of channel properties
+%                   .amp        -   index of amplifier channel (if there is one)
+%                   .mode       -   index of mode telegraph channel (if there is one)
+%                   .gain       -   index of gain telegraph channel (if there is one)
+%
+%   $Id$
+
+d = daqread(filename,'info');
+info.t_unit = 's';
+info.t_rate = d.ObjInfo.SampleRate;
+info.start_time = d.ObjInfo.InitialTriggerTime;
+info.samples = d.ObjInfo.SamplesAcquired;
+info.channels = d.ObjInfo.Channel;
+cnames = {info.channels.ChannelName};
+info.amp = strmatch('amplifier',cnames);
+info.mode = strmatch('mode',cnames);
+info.gain = strmatch('gain',cnames);
+
+% make sure there is an amp channel
+if isempty(info.amp)
+    info.amp = 1;
+end
