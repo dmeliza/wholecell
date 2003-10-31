012b506e7efabb7a7624cab90182707eeb14c80c
meliza <>
2003-10-31 19:16:43 +0000

    bindata generalized to work with more than 2 dimensions, with help from new friend SliceRef

--- Analysis/BinData.m
+++ Analysis/BinData.m
@@ -1,6 +1,6 @@
-function binneddata = BinData(data, binfactor)
-% Bins data in a 2-dim matrix arranged columnwise into bins of binfactor columns
-% For example: Matrix A is:
+function data = BinData(data, binfactor, dimension)
+% Generalized data binning function.  The default is to bin along dimension 2,
+% which given Matrix A:
 % [1 2 3 4]
 % [2 3 4 5]
 % [5 6 7 8]
@@ -8,31 +8,65 @@ function binneddata = BinData(data, binfactor)
 % [1.5 3.5]
 % [2.5 4.5]
 % [5.5 7.5]
+% while BinData(A,2,1) would give
+% [1.4 2.5 3.5 4.5]
+% [ 5   6   7   8 ]
 %
-% note that incomplete bins are discarded, so pick a binfactor as close
-% as possible to a factor of size(data, 2)
 %
 % 1.3 - now attempts to detect if the input is a column
+% 1.4 - generalized to any number of dimensions, modulus is preserved
 %
 % $Id$
 
-lT = size(data, 2);
-if lT == 1 % data is probably columnwise
-    data = shiftdim(data, 1);
-    lT = size(data,2);
+error(nargchk(2,3,nargin))
+
+if nargin < 3
+    dimension = 2;
 end
-traceCount = fix(lT / binfactor); % throws away modulus
-lT = traceCount * binfactor;
 
-% reshape method
-nd = reshape(data(:,1:lT),size(data,1), binfactor, traceCount);
-binneddata = squeeze(mean(nd,2));
+ndim = ndims(data);
+dims = size(data);
 
-% iterative method
-% binneddata = zeros(size(data,1), traceCount);
-% for i = 1:traceCount
-%     o = i*binfactor;
-%     d = data(:,o:(o+binfactor));
-%     binneddata(:,i) = mean(d,2);
-% end
-    
+if dimension > ndim
+    error(['Input matrix has only ' num2str(ndim) ' dimensions!']);
+end
+
+% catch column vectors, which don't bin properly with the default dimension
+if ndim == 2 & dims(2) == 1
+    dimension = 1;
+end
+
+% we have to permute the dimensions of the input array so that the values will
+% be read in correctly by reshape
+neworder = [dimension setdiff(1:ndim,dimension)];
+data     = permute(data, neworder);
+dims     = dims(neworder);
+
+% compute the shape of the final matrix & extract the modulus
+row  = floor(dims(1) / binfactor);         % number of hyperrows in the binned dimension
+last = dims(1) - mod(dims(1),binfactor);   % the index of the last evenly divisible hyperrow
+if last < dims(1)
+    nd   = SliceRef(data,1,1:last);               % the reshapable portion of the matrix
+    mod  = SliceRef(data,1,last+1:dims(1));       % the modulus
+else
+    nd   = data;
+    mod  = [];
+end
+
+% bin the data by reshaping and averaging the matrix
+nd              = reshape(nd,[row binfactor dims(2:end)]);
+[nd,nshift]     = shiftdim(mean(nd,2));
+if nshift > 1
+    nd          = shiftdim(nd,-1);  % recover leading singleton dimension if necc.
+else
+    nd          = squeeze(nd);      % eliminate internal singletons
+end
+
+% compute binned modulus here
+if ~isempty(mod)
+    mod    = mean(mod,1);
+    nd     = cat(1,nd,mod);
+end
+
+% permute the matrix back to its correct shape
+data   = ipermute(nd, neworder);
---
+++ Analysis/SliceRef.m
@@ -0,0 +1,34 @@
+function out = SliceRef(A, dim, slice)
+%
+% A more generalized way to retrieve a slice or slices from a multi-dimensional
+% array. Useful when the number of dimensions in the array are variable.
+% 
+% If A is a 4-D array:
+% SliceRef(A, 3, 1:3) == A(:,:,1:3,:);
+% SliceRef(A, 4, 10) == A(:,:,:,10);
+%
+% Usage: out = SliceRef(array, dim, slice)
+%
+%   array   - the array to be sliced
+%   dim     - the dimension on which to take the slice
+%   slice   - a vector or scalar defining which slices to take
+%
+%   out     - the slice or slices
+%
+error(nargchk(3,3,nargin))
+dims = ndims(A);
+
+% construct reference string
+s = '';
+for i = 1:dims
+    if i == dim
+        ins = 'slice';
+    else
+        ins = ':';
+    end
+    s = sprintf('%s%s,',s,ins);
+end
+s = s(1:end-1); % remove trailing comma
+sf = sprintf('A(%s)',s);
+
+out = eval(sf);
