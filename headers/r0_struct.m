47ba35ae7e9e072e45699c10b2fc0e5d2dbf2a64
meliza <>
2003-11-04 18:59:26 +0000

    *** empty log message ***

---
+++ headers/r0_struct.m
@@ -0,0 +1,25 @@
+function [s, fields] = r0_struct()
+%
+% Defines the r0 structure (by returning a structure with the proper fields)
+% r0 structures are used to describe episodic responses to periodic stimulation.
+% Each episode is required to be the same length, and the relative timing of
+% each episode is also required.
+%
+% Required fields:
+%
+% .data     - the response, which is an NxMxP array of M episodes of N samples,
+%              with P channels of data
+% .time     - an Nx1 vector describing the time offset of each sample (in seconds)
+% .abstime  - an Mx1 vector describing the time offset of each episode (in minutes)
+% .t_rate   - the sampling rate (in Hz) of the data (redundant with .time)
+% .y_unit   - a character array (P rows) describing the units of each channel
+%
+% Optional fields:
+%
+% .info     - data from the source file
+%
+% $Id$
+
+fields = {'data','time','abstime','t_rate','y_unit'};
+C      = {[],[],[],[],''};
+s      = cell2struct(C, fields, 1);
---
+++ headers/r1_struct.m
@@ -0,0 +1,25 @@
+function [s, fields] = r1_struct()
+%
+% Defines the r1 structure (by returning a structure with the proper fields)
+% r1 structures are used to describe multiple responses to a similar stimulation.
+% Unlike r0 structures, no constraint is placed on response length, and the
+% relative timing of each episode is ignored.  r1 structures are implemented
+% as structure arrays, with each element of the array representing a single repeat
+%
+% Required fields:
+%
+% .data     - the response, which is an NxP array of N samples with P channels of data
+% .timing   - a synchronization "channel" which represents the times at which
+%           - the stimulus changed frames
+% .t_rate   - the sampling rate (in Hz) of the data
+% .y_unit   - a character array (P rows) describing the units of each channel
+%
+% Optional fields:
+%
+% .info     - data from the source file
+%
+% $Id$
+
+fields = {'data','timing','t_rate','y_unit'};
+C      = {[],[],[],''};
+s      = cell2struct(C, fields, 1);
