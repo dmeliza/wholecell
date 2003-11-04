b8de1d871cfbf3e1abae16ad134093c01d2b73de
meliza <>
2003-11-04 22:10:14 +0000

    returns a character array for multiple selections

--- Utility/GetSelected.m
+++ Utility/GetSelected.m
@@ -1,11 +1,16 @@
 function out = GetSelected(list_handle)
-% Returns the string selected in a list uicontrol
+% Returns the string selected in a list uicontrol. If multiple items
+% are selected, a character array is returned.
+%
+% Usage: out = GetSelected(list_handle)
+%
+% list_handle - graphics object handle for the list
 %
 % $Id$
 i = get(list_handle,'Value');
 s = get(list_handle,'String');
 if iscell(s)
-    out = s{i};
+    out = char({s{i}});
 elseif i <= length(s)
     out = s(i,:);
 else
