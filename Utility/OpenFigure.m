fa1422f44070950f90753258f7ec423ac1899bc1
meliza <>
2003-11-04 18:28:17 +0000

    OpenFigure now uses FindFig to open the new figure

--- Utility/OpenFigure.m
+++ Utility/OpenFigure.m
@@ -1,11 +1,20 @@
 function fig = OpenFigure(module,varargin)
-% Opens a blank figure window 
-% void OpenFigure(module,[prop1,val1],[prop2,val2],[...])
-% sets properties to values if supplied
+% Opens a blank figure window, or if a figure with that handle already exists,
+% returns the handle to it.
+%
+% Usage: handle =  OpenFigure(module,[prop1,val1],[prop2,val2],[...])
+%
+% module - the name of the module (used for the tag and for the wc structure)
+% propn  - property name
+% valn   - the corresponding value
+%
+% $Id$
 global wc
 
-obj.fig = figure('numbertitle','off','name',module,'tag',module,...
-             'DoubleBuffer','on','menubar','none');
+% open or find the figure
+obj.fig = findfig(module);
+% set some default values
+set(obj.fig, 'numbertitle','off','name',module,'DoubleBuffer','on','menubar','none');
 set(obj.fig,'Color',get(0,'defaultUicontrolBackgroundColor'));
 obj.handles = guihandles(obj.fig);
 guidata(obj.fig, obj.handles);
