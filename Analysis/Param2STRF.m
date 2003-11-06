13dffd0ca1621d4408d37fe04925fd36d6199f5e
meliza <>
2003-11-06 18:46:43 +0000

    simplified and cleaned up, only uses one set of parameters now (interface changed)

--- Analysis/Param2STRF.m
+++ Analysis/Param2STRF.m
@@ -1,6 +1,6 @@
-function [strf_on, strf_off] = Param2STRF(responses, parameters, on, off)
+function [strf] = Param2STRF(responses, parameters)
 %
-% Converts an NxM array of impulse responses into two IxJxM arrays
+% Converts an NxM array of impulse responses into an IxJxM arrays
 % which are the spatial-temporal receptive fields (ON and OFF respectively)
 % The "real" stimuli that are generated are the same as a sparse noise frame
 %
@@ -10,35 +10,30 @@ function [strf_on, strf_off] = Param2STRF(responses, parameters, on, off)
 % responses        - NxM array. N parameters, M time points
 % parameters       - NxIxJ array. N parameters, IxJ stimulus
 %                    to map from parameter values to real stimuli
-% on               - indices of "ON" stimuli
-% off              - indices of "OFF" stimuli
+%
+% strf             - IxJxM spatiotemporal receptive field
 %
 % $Id$
 
-error(nargchk(4,4,nargin))
+error(nargchk(2,2,nargin))
 
 [PARAMS SAMPLES] = size(responses);
 [N I J] = size(parameters);
 
 if PARAMS ~= N
     error('Number of parameters must be the same in response and parameter arrays');
-elseif length(on) + length(off) ~= PARAMS
-    error('ON and OFF vectors must add up to the number of parameters');
 end
 
 % condition parameters to zero mean
 m = mean(mean(mean(parameters)));
 parameters = parameters - repmat(m,size(parameters));
 
-strf_on = zeros(I,J,SAMPLES);
-strf_off = zeros(I,J,SAMPLES);
-ON = parameters(on,:,:);
-OFF = parameters(off,:,:);
+% allocate output
+strf = zeros(I,J,SAMPLES);
 
+% loop through timeframes and assign values to each parameter
 for i=1:SAMPLES
-    R = responses(:,i); % response to each parameter at time i
-    R_ON = repmat(R(on),[1,I,J]);
-    R_OFF = repmat(R(off),[1,I,J]);
-    strf_on(:,:,i) = mean((R_ON .* ON),1);
-    strf_off(:,:,i) = mean((R_OFF .* OFF),1);
+    R           = responses(:,i);            % response to each parameter at time i
+    R           = repmat(R,[1,I,J]);         % response mapped to each pixel
+    strf(:,:,i) = mean((R .* parameters),1); % response * parameter value at each pixel
 end
