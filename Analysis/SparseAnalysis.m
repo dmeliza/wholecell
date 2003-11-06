bceca05961322d3d9185367f37ad0fd977501387
meliza <>
2003-11-06 21:50:05 +0000

    cleanup to save memory

--- Analysis/SparseAnalysis.m
+++ Analysis/SparseAnalysis.m
@@ -41,42 +41,36 @@ if nargin < 4
     bin = 0;
 end
 
-%initFigure;
-analyze(stim, resp, window, bin);
-
-function [] = initFigure()
-% Initializes the figure
-f   = figure;
-movegui(f,'northwest');
-set(f,'Color',[1 1 1],'Name','Sparse Analysis','NumberTitle','off');
-%'Position',[360 314 560 520]
-
-function [] = analyze(stim, resp, window, bin)
 % stimulus-param mappings and the parameter vector
 [stimulus param params] = unique(stim.parameters,'rows'); % all the unique combinations of values
-stimulus = binarystimulus(stimulus);                   % make z values 0 and 1
-ind_on   = find(stimulus(:,end));                         % ON parameters
-ind_off  = find(stimulus(:,end)==0);                      % OFF parameters
+parameters = stim.stimulus(:,:,param);                     % 2D arrays for each stimulus
+stimulus   = binarystimulus(stimulus);                     % make z values 0 and 1
+ind_on     = find(stimulus(:,end));                         % ON parameters
+ind_off    = find(stimulus(:,end)==0);                      % OFF parameters
 
-% allocate output array
+% initialize output arrays
 repeats  = length(resp);
-h1_est   = zeros(length(param), window);
 R        = [];
 P        = [];
+clear('stim');                                   % clear up some memory
 
 % loop through each of the sweeps
 S = warning('off');                                     % frameshift can throw warnings
 for i = 1:repeats
     fprintf('Sweep %d: ', i);
-    r       = double(resp(i).data);
-    timing  = resp(i).timing;
 
     % frame shift response
     fprintf('Conditioning response... \n');
-    r    = FrameShift(r,timing,window,'correct');         % frame shift data
-    len  = size(r,1);                                      % number of parameters we can look at
-    R    = cat(1,R,r);                                     % response matrix
-    P    = cat(1,P,params(1:len));                         % parameter vector
+    r     = FrameShift(double(resp(i).data),...
+                      resp(i).timing,...
+                      window,'correct');                   % frame shift data
+    len   = size(r,1);                                      % number of parameters we can look at
+    
+    if bin > 1
+        r = BinData(r,bin,2);                               % binning data now saves a lot of memory
+    end
+    R     = cat(1,R,r);                                     % response matrix
+    P     = cat(1,P,params(1:len));                         % parameter vector
 end
 warning(S);
 
@@ -84,10 +78,10 @@ warning(S);
 fprintf('Parameterizing response...\n');
 h1_est        = Parameterize(P,R);          % combine and average
 % bin the result if neccessary
-if bin > 1
-    h1_est    = BinData(h1_est,bin,2);
-    R         = BinData(R,bin,2);
-end
+% if bin > 1
+%     h1_est    = BinData(h1_est,bin,2);
+%     R         = BinData(R,bin,2);
+% end
 cb       = @clickme;
 frate    = getpref('strfGUI','srate');
 PlotParams(struct('data',{h1_est(ind_on,:),h1_est(ind_off,:)},...
@@ -98,10 +92,9 @@ movegui(gcf,'northwest');
 
 % generate STRF and plot it
 fprintf('Computing STRF...\n');
-stimulus = stim.stimulus(:,:,param);                     % 2D arrays for each stimulus
-stimulus = permute(stimulus,[3 1 2]);                    % reshape for use with STRF
-ON       = Param2STRF(h1_est(ind_on, :), stimulus(ind_on, :,:));
-OFF      = Param2STRF(h1_est(ind_off,:), stimulus(ind_off,:,:));
+P        = permute(parameters,[3 1 2]);                    % reshape for use with STRF
+ON       = Param2STRF(h1_est(ind_on, :), P(ind_on, :,:));
+OFF      = Param2STRF(h1_est(ind_off,:), -P(ind_off,:,:)); % invert off parameters
 frate    = getpref('strfGUI','srate');
 pos      = get(gcf,'Position');
 PlotSTRF(struct('data',{ON,OFF},'title',{'ON','OFF'},'frate',{frate,frate}));
