baf75170b2345560321fdfd409eadf768feaf36b
lab <>
2003-12-10 22:19:31 +0000

    modified colormap handling to ensure black white flips on sync rect and nice white text

--- Visual/CgPlayFrames.m
+++ Visual/CgPlayFrames.m
@@ -1,10 +1,10 @@
-function CgPlayMovie(frate, syncrect, syncmap)
+function CgPlayFrames(frate, syncrect)
 % Plays a movie using the coggraph toolkit.  We define a movie
 % as a collection of sprites that are played at a certain frame
 % rate.  This function requires the movie to have been loaded into
 % video memory, and will throw an error if there are not enough sprites loaded
 %
-% CgPlayMovie(frate, [syncrect, [syncmap]])
+% CgPlayMovie(frate, [syncrect])
 % frate - the frame rate of the movie, in multiples of the refresh time
 %         (e.g. if the refresh rate is 60 Hz and frate is 2, the frame rate
 %          will be 30 Hz)
@@ -12,11 +12,13 @@ function CgPlayMovie(frate, syncrect, syncmap)
 %            (that is, it changes state every frame). The dimensions are in fractions
 %            of the total screen size, with the origin at the bottom left.
 %            Default is [0 0 .125 .125]
-% syncmap - 1X2 array defining the two colmp lookups for the on and off state
-%           of the sync rectangle.
-%           default is [0 1]
 %
-% TODO: Restrict movie to a region of the screen; make pixels non-ortho to screen.
+% Changes:
+% 1.8:      This function now commandeers colormap entries 0 and 255 for the sync
+%           rectangle and the status text.  These will be set to [0 0 0] and [1 1 1]
+%           respectively.  Consequently, the movie colormap can only have 254 values,
+%           as it will be loaded starting in position 1, and if a 254th value is
+%           supplied, it will be overwritten by CgPlayFrames
 %
 % $Id$
 global timing;
@@ -48,12 +50,16 @@ if nargin < 2
 end
 sr = (syncrect + [-1 -1 0 0]) .* [PW/2 PH/2 PW PH];
 if nargin < 3
-    syncmap = [0 1];
+    syncmap = [0 255];
 end
-% bombs away
+% set font and colormap
 cgfont('Arial',10)
-cgpencol(1) % should be white
+cgcoltab(0,[0 0 0])
+cgcoltab(255,[1 1 1])
+cgnewpal
+cgpencol(255)
 t = [PW/2 - 100, -PH/2 + 20];
+% bombs away
 for frame = 1:a_frames;
     for i = 1:frate
         cgdrawsprite(frame,x,y, pw, ph);
--- Visual/CgQueueMovie.m
+++ Visual/CgQueueMovie.m
@@ -10,10 +10,17 @@ function CgQueueMovie(movie, a_frames)
 %         fields defined (see LoadMovie for details on this structure)
 % a_frames - the number of frames to load (default = size(movie.stimulus,3))
 %
+% Note that the movie's colormap is loaded into slots 1-255, meaning that the movie
+% can only contain 254 CLUT values.  Futhermore, entry 255 will be set to [1 1 1],
+% so non-grayscale movies that use 255 will give unexpected results.
+%
 % $Id$
 
 % setup colormap:
-cgcoltab(0,movie.colmap);
+if size(movie.colmap,1) > 254
+    movie.colmap = movie.colmap(:,1:254);
+end
+cgcoltab(1,movie.colmap);
 cgnewpal;
 % load sprites:
 pix = movie.x_res * movie.y_res;
@@ -24,7 +31,7 @@ end
 h = waitbar(0,['Loading movie (' num2str(a_frames) ' frames)']);
 for i = 1:a_frames
     s = cgremap(movie.stimulus(:,:,i));
-    cgloadarray(i,movie.x_res,movie.y_res,s,movie.colmap,0);
+    cgloadarray(i,movie.x_res,movie.y_res,s,movie.colmap,1);
     waitbar(i/a_frames,h);
 end
 close(h);
