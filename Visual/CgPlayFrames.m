22f3ce5aa95ec24938871b5ab2556aa8dba0afb9
lab <>
2003-12-16 18:34:44 +0000

    optimization attempts

--- Visual/CgPlayFrames.m
+++ Visual/CgPlayFrames.m
@@ -73,11 +73,12 @@ else
         Z     = feval(s1.mfile,s1.static{:},s1.param(frame,:));
         [X,Y] = size(Z);
         cgloadarray(2,X,Y,reshape(Z',1,X*Y),s1.colmap,1);
-        for i = 1:frate
-            cgdrawsprite(2,x,y, pw, ph);
-            cgrect(sr(1),sr(2),sr(3),sr(4),syncmap(sync+1));
-            cgtext(num2str(frame),t(1),t(2));
-            cgflip(0);
+        cgdrawsprite(2,x,y, pw, ph);
+        cgrect(sr(1),sr(2),sr(3),sr(4),syncmap(sync+1));
+        cgtext(num2str(frame),t(1),t(2));
+        cgflip(0);
+        for i = 1:frate-1
+            cgnewpal;                   % this should be sync'd with the display
         end
         sync = ~sync;
     end
--- Visual/HartleyGrating.m
+++ Visual/HartleyGrating.m
@@ -15,9 +15,15 @@ function img = hartleygrating(dim, k)
 % of the vectors k = (kx,ky) and r = (l,m) (in Cartesian coordinates).  There
 % are undoubtedly faster ways to implement this.
 %
+% Values are quantized to 254 discrete CLUT values.  These constants are hardcoded
+% for speed
+%
 % $Id$
 
 error(nargchk(2,2,nargin))
+
+MINCOL  = 1;
+MAXCOL  = 254 - 1;
 
 sz    = dim.*dim;
 l     = 0:dim-1;
@@ -25,6 +31,10 @@ l     = 0:dim-1;
 R     = [reshape(L,sz,1),reshape(M,sz,1)];  % R matrix
 
 Z     = R * k' * 2 * pi / dim;         % inner product
-img   = sin(Z) + cos(Z);
+img   = sin(Z) + cos(Z);               % P2P tends to be around 1.4
+img   = round(img * MAXCOL/2/(-min(img)) + MAXCOL/2 + MINCOL);
+%img   = img * 90.7 + 128;              % img * MAXCOL/2/-min(img) + MAXCOL/2 + MINCOL;
+% img   = img - min(img);
+% img   = round(img * (MAXCOL - MINCOL) / max(img) + MINCOL);
 img   = reshape(img,dim,dim);
 
