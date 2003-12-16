719728ccf1f38d0f9bf6160ace267b3b4e64edfe
meliza <>
2003-12-16 20:29:59 +0000

    fixes so that this works with HartleyGrating()

--- Visual/GratingSubspace.m
+++ Visual/GratingSubspace.m
@@ -1,38 +1,51 @@
 function s1 = GratingSubspace(dim,wn,num,repeats)
 %
 % Generates a complete sampling of the hartley basis set between
-% between a minimum and a maximum wavenumber.
+% between a minimum and a maximum wavenumber, using all the integral wavenumbers
+% between [0,wn].
 %
 % USAGE: s0 = GratingSubspace(dim,wn,[num,[repeats]])
 %
 % dim       - size of square images to generate
 % wn        - max wavenumber
-% num       - the number of kx or ky values to generate (# of parameters = num^2)
-%             default is 10
+% num       - how densely to saturate the plane. default is 1, which includes
+%             every integral wavenumber between -wn and +wn.  If this is 2,
+%             every other value will be skipped, etc.
 % repeats   - the number of sequences to generate (default 1)
 % 
 % s1 - stimulus structure
+%
+% Changes:
+%
+% the FHT used now by HartleyGrating() only accepts integral wavenumbers, so
+% we check for that and only generate integral parameter values now
 %
 % $Id$
 
 MFILE   = 'hartleygrating';
 
+% check arguments
 error(nargchk(0,4,nargin))
 if nargin < 2
     [dim, wn, num, repeats] = ask;
 elseif nargin < 3
-    num     = 10;
+    num = 1;
+    repeats = 1;
 elseif nargin < 4
-    num     = 10;
     repeats = 1;
 end
+dim = fas(dim);
+num = fas(num);
+wn  = fas(wn);
+repeats = fas(repeats);
+
 
 MINCOL  = 1;       % minimum CLUT value
 MAXCOL  = 255;     % maximum CLUT value
 
-kx      = -wn:2*wn/(num-1):wn;
+kx      = -wn:num:wn;
 ky      = kx;
-frames  = num*num;
+frames  = length(kx)^2;
 
 % parameter space:
 [X Y]   = meshgrid(kx, ky);
@@ -52,12 +65,16 @@ function [dim, frequency, num, repeats] = ask()
 % opens a dialog box to ask values
 prompt = {'Resolution (pixels):',...
         'Max Spatial Frequency (wavenumber):',...
-        'Number of frequencies:',...
+        'Tiling density (1 == all):',...
         'Sequence Repeats:'};
-def = {'100','9','10','1'};
+def = {'100','9','1','1'};
 title = 'Values for 2D Gratings';
 answer = inputdlg(prompt,title,1,def);
 dim = str2num(answer{1});
 frequency = str2num(answer{2}); 
 num = str2num(answer{3});
 repeats = str2num(answer{4});
+
+function num = fas(in)
+% converts a number into an unsigned integer
+num = fix(abs(in));
