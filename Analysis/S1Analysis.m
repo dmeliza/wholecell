1c46b9f76593792ea9c6eb33340d7656af4cae6d
meliza <>
2004-01-07 22:56:00 +0000

    uses new a1 structure

--- Analysis/S1Analysis.m
+++ Analysis/S1Analysis.m
@@ -1,4 +1,4 @@
-function h1_est = S1Analysis(stim, resp, window, bin)
+function a1 = S1Analysis(stim, resp, window, bin)
 % This function performs a sparse-noise analysis of a stimulus/response pair.
 % It's the next generation from SparseAnalysis(), in that it uses the parametric
 % s1 stimulus structure. Sparse noise analysis works by computing the mean responses
@@ -14,7 +14,7 @@ function h1_est = S1Analysis(stim, resp, window, bin)
 % responses to a parameter set, these are averaged.
 %
 %
-% Usage: h1_est = S1Analysis(s1, r1, [window, [bin]])
+% Usage: a1 = S1Analysis(s1, r1, [window, [bin]])
 %
 % s1     - the s1 stimulus structure
 % resp   - the response structure (r1 format)
@@ -23,14 +23,17 @@ function h1_est = S1Analysis(stim, resp, window, bin)
 % bin    - the amount to bin the resulting response vectors, in samples.  If 0 or 1,
 %          no binning will be done.  Default is 0
 %
-% h1_est    -  an NxM array describing the first M samples following the presentation of
-%              a stimulus parameter (whose value can range from 1 to N). Repeats are
-%              during the frameshift stage, and the individual "responses" to each
-%              parameter are preserved.
+% a1    -  the results of the analysis, in an a1 structure.  The kernel is an NxM
+%          array describing the first M frames of the average response following
+%          presentation of the stimulus defined by the Nth parameter. Contains the
+%          optional .param and .strf fields.
 % 
 % See Also:
 %           headers/s1_struct.m
 %           headers/r1_struct.m
+%           headers/a1_struct.m
+%           Analysis/FrameShift.m (creates a frame-shifted response array - similar to buffer())
+%           Analysis/Parameterize.m (sorts response array by stimulus parameters)
 %           Analysis/Param2STRF (converts param array into STRF)
 %           Analysis/PlotSTRF.m (used to view/analyze STRF)
 %           Analysis/PlotParams.m (used to view/analyze parameter responses)
@@ -49,12 +52,12 @@ end
 
 % find all the unique parameter combinations
 [stimulus param params] = unique(stim.param,'rows'); % all the unique combinations of values
+a1.param = stimulus;
 
 % initialize output arrays
 repeats  = length(resp);
 R        = [];
 P        = [];
-%clear('stim');                                   % clear up some memory
 
 % loop through each of the sweeps
 S = warning('off');                                     % frameshift can throw warnings
@@ -62,7 +65,7 @@ for i = 1:repeats
     fprintf('Sweep %d: ', i);
 
     % frame shift response
-    fprintf('Conditioning response... \n');
+    fprintf('Frameshifting response... \n');
     r     = FrameShift(double(resp(i).data),...
                       resp(i).timing,...
                       window,'correct');               % frame shift data
@@ -74,17 +77,36 @@ for i = 1:repeats
     R     = cat(1,R,r);                                     % response matrix
     P     = cat(1,P,params(1:len));                         % parameter vector
 end
-warning(S);
+warning(S);                                
+
 % parameterize response
 fprintf('Parameterizing response...\n');
-h1_est   = Parameterize(P,R);          % combine and average
-cb       = @clickme;
-frate    = getpref('strfGUI','srate');
-PlotParams(struct('data',h1_est,...
-                  'title','Parameters',...
-                  'frate',frate,...
-                  'cb',{{cb,R,P}}));
-movegui(gcf,'northwest');              
+a1.kern      = Parameterize(P,R);          % combine and average
+a1.frate     = getpref('strfGUI','srate');
+if nargout == 0                        % no display if output values are assigned
+    cb       = @clickme;
+    PlotParams(struct('data',a1.kern,...
+        'title','Parameters',...
+        'frate',frate,...
+        'cb',{{cb,R,P}}));
+    movegui(gcf,'northwest');
+end
+
+
+% check if we can convert to STRF
+err = 0;
+if isfield(stim,'mfile')
+    [path func] = fileparts(stim.mfile);
+    if ~exist(func)
+        err = 1;
+    end
+else
+    err = 1;
+end
+if err
+    warning('Unable to load conversion function.  No STRF computed.');
+    return
+end
 
 % Now we have to convert the parameterized responses into a "real" STRF.  The most
 % general way to do this is to use the mfile specification in the s1 structure to
@@ -92,12 +114,6 @@ movegui(gcf,'northwest');
 % the values of the parameterized strf.  Of course, there are more elegant ways
 % to transform particular basis sets, but this should be about as unbiased as we can get.
 fprintf('Computing STRF...\n');
-mf = stim.mfile;
-[path func] = fileparts(mf);
-if ~exist(func)
-    errordlg(['Cannot find ' func ': no STRF generated']');
-    return
-end
 fp  = stim.static;                                      % function parameters
 sz  = length(param);                                    % size of basis set
 uv  = zeros(stim.x_res,stim.y_res,sz);                  % pre-allocate
@@ -105,13 +121,16 @@ for i = 1:sz
     uv(:,:,i) = feval(func, fp{:}, stimulus(i,:));      % generate unit vector
 end
 uv   = reshape(uv, prod([stim.x_res,stim.y_res]),sz);   % reshape to pixel X param
-strf = uv * h1_est;                                     % matrix multiply to eliminate param
-strf = reshape(strf,stim.x_res,stim.y_res,size(h1_est,2));
-PlotSTRF(struct('data',strf,'title','STRF','frate',frate));
-pos      = get(gcf,'Position');
-movegui(gcf,[pos(1) pos(2)-340])
+strf = uv * a1.kern;                                    % matrix multiply to eliminate param
+a1.strf = reshape(strf,stim.x_res,stim.y_res,size(a1.kern,2));
 
-% 
+% display only if output values are unassigned
+if nargout == 0                                         
+    PlotSTRF(struct('data',strf,'title','STRF','frate',frate));
+    pos      = get(gcf,'Position');
+    movegui(gcf,[pos(1) pos(2)-340])
+end
+ 
 function [] = clickme(obj, event, R, P)
 % handles double clicks on sparse analysis window
 type    = get(gcf,'selectiontype');
@@ -134,5 +153,4 @@ if strcmpi(type,'open');
     mtrialplot(lim,resp');
     xlabel('Time (s)')
     title(str);
-    %plot(s.k(y,:));
 end
