a0e3fe7b4ebb950463e2f0d8a69be656844ed68e
meliza <>
2004-01-08 20:02:15 +0000

    massive rewrite. supports dual channel. still a lot of commented out code

--- RevCorr_LED.m
+++ RevCorr_LED.m
@@ -1,25 +1,29 @@
 function varargout = RevCorr_LED(varargin)
 %
-% This protocol uses a flashing LED to determine the temporal
-% receptive field of a cell in current or voltage clamp.  The voltage to
-% the LED is output (gaussian white noise) and the signal from the amplifier
-% is recorded.  The LED signal is stored for reverse correlation analysis.
-%
-% Online analysis looks for the first moment of the temporal filter (TBI).
+% RevCorr_LED: A protocol for use with the wholecell toolkit.  Control up to
+% two LED's with varying degrees of correlation.  Intended for monocular
+% or binocular full-field stimulation of an animal while recording.  The user can
+% select the frame rate of each LED and a file or mfile that describes the time sequence
+% of each LED's activity.
+% 
+% Online analysis uses xcorr to look for the first moment of the temporal filter.
 %
 % void RevCorr_LED(action)
 %
-% action is {'init'} 'play', 'record', or 'stop'
-% other actions are used as internal callbacks
+% Input:  a file containing one or two column vectors which represent the state of
+%         each LED over the course of the acquisition.  Or an mfile that dynamically
+%         generates these sequences.
 %
-% parameters:
-% (output)
-%     - s_max: maximum output voltage to LED
-%     - s_res: number of distinct output voltages
-%     - t_res: frame rate of LED
-% (analysis)
-%     - s_len: length of stimulus to consider for rev corr
-%     - r_thresh: minimum response to be considered a response
+% Output: The DAQ toolkit is responsible for sending the voltages to the LED's through
+%         the analogoutput object, and recording the response of the cell through
+%         the analoginput object.
+%
+% Details: The operation of this protocol is quite straightforward.  The user specifies
+%          a frame rate and an output sequence.  The sequence is loaded into the daq's
+%          analog output, and when the user clicks "Start" or "Record", the ai and ao
+%          objects are triggered simultaneously.  Empirically, synchronization is very
+%          tight between the input and output, so no additional signal is needed to
+%          align the stimulus waveform with the response.
 %
 % $Id$
 
@@ -40,12 +44,11 @@ switch action
 
 case {'init','reinit'}
     p = defaultParams;
-    fig = OpenParamFigure(me, p);
-    Scope('init');
+    fig = ParamFigure(me, p);
+    getScope;
     
 case 'start'
     setupHardware;
-    setupScope;
     llf = get(wc.ai,'LogFileName');
     [dir newdir] = fileparts(llf);
     set(wc.ai,'LogFileName',fullfile(dir, '0000.daq'));    
@@ -58,7 +61,6 @@ case 'record'
         feval(me,'stop');
     end
     setupHardware;
-    setupScope;
     llf = get(wc.ai,'LogFileName');
     [dir newdir] = fileparts(llf);
     s = mkdir(dir, newdir);
@@ -91,10 +93,8 @@ function p = defaultParams()
 global wc;
 
     f = {'description','fieldtype','value','units'};
-    p.output.description = 'LED channel';
-    p.output.fieldtype = 'list';
-    p.output.choices = GetChannelList(wc.ao);
-    p.output.value = 1;
+    p.sequence = struct('description','Sequence','fieldtype','file_in',...
+                        'value','');
     p.s_max.description = 'Max LED Voltage';
     p.s_max.fieldtype = 'value';
     p.s_max.units = 'V';
@@ -103,16 +103,14 @@ global wc;
     p.s_min.fieldtype = 'value';
     p.s_min.units = 'V';
     p.s_min.value = 2.0;
-    p.s_res.description = 'Stim Res';
-    p.s_res.fieldtype = 'value';
-    p.s_res.value = 5;
-    p.t_res.description = 'Frame Rate';
-    p.t_res.fieldtype = 'value';
-    p.t_res.units = 'ms';
-    p.t_res.value = 100;
-    
-    p.d_rate = cell2struct({'Display Rate','value',10,'Hz'},f,2);
-    p.a_int = cell2struct({'Analyze every:','value',30,'s'},f,2);
+%     p.a_int = struct('description','Repeat Length','fieldtype','value',...
+%                       'value',50,'units','s');
+    p.repeat = struct('description','Repeats (0=inf)','fieldtype','value',...
+                      'value',0.2);
+    p.f_rate = struct('description','Frame Rate','fieldtype','value',...
+                     'units','Hz','value',20);
+%     p.d_rate = struct('description','Display Rate','fieldtype','value',...
+%                       'value',10,'units','Hz');
     p.input.description = 'Amplifier Channel';
     p.input.fieldtype = 'list';
     p.input.choices = GetChannelList(wc.ai);
@@ -123,148 +121,30 @@ global wc;
 function setupHardware()
 % Sets up the hardware for this mode of acquisition
 global wc
-display = @updateDisplay;
+%display = @updateDisplay;
 analyze = @analyze;
-sr = get(wc.ai, 'SampleRate');
-u_rate = GetParam(me,'d_rate','value');
-a_int = sr * GetParam(me,'a_int','value');
-update = fix(sr / u_rate); 
+srate   = get(wc.ai, 'SampleRate');               % sample rate of ai
+frate   = GetParam(me,'f_rate','value');           % frame rate of LED in Hz
+%d_rate  = GetParam(me,'d_rate','value');          % update rate, Hz
+len     = queueSequence(wc.ai, wc.ao);            % number of frames
+a_int   = sr * GetParam(me,'a_int','value');      % analysis interval, samples
+%update = fix(sr / u_rate); 
 set(wc.ai,'SamplesPerTrigger',a_int);
-set(wc.ai,'TimerPeriod',1 / u_rate);
-set(wc.ai,'TimerAction',{me,display})
+%set(wc.ai,'TimerPeriod',1 / u_rate);
+%set(wc.ai,'TimerAction',{me,display})
 set(wc.ai,'SamplesAcquiredActionCount',a_int);
 set(wc.ai,'SamplesAcquiredAction',{me,analyze});
 set(wc.ai,'DataMissedAction',{me,'showerr'});
-set(wc.ai,'UserData',update);
+%set(wc.ai,'UserData',update);
 set([wc.ai wc.ao],'TriggerType','Manual');
 set(wc.ai,'ManualTriggerHwOn','Trigger');
 
-t_res = GetParam(me,'t_res','value');
-sr = 1000 / t_res ;
-set(wc.ao, 'SampleRate', sr);
+set(wc.ao, 'SampleRate', frate);
+% set(wc.ao,'SamplesOutputFcnCount', sr * a_int * 19);
+% set(wc.ao,'SamplesOutputFcn',{me, requeue});
 
-Spool('stim','init');
-Spool('resp','init');
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%55
-function queueStimulus()
-% queues data in the ao object and records it to a param in wc
-global wc
-t_res = GetParam(me,'t_res','value');
-a_int = GetParam(me,'a_int','value');
-update = a_int * 1000 / t_res * 1.2;
-queued = get(wc.ao,'SamplesAvailable');
-if (queued < 0.1 * update)
-    control = GetParam(me,'output','value');
-    c = zeros(update, length(wc.ao.Channel));
-    wn =  whitenoise(update);
-    c(:,control) = wn;
-    putdata(wc.ao, c);
-    Spool('stim', 'append', wn');
-    m = lower(get(wc.ai,'LoggingMode'));
-    if ~strcmp('memory',m)
-        lf = get(wc.ai,'Logfilename');
-        writeStimulus(lf, wn, 1000 / t_res, a_int);
-    end
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function wn = whitenoise(samples)
-% generates quantized one-dimensional gaussian white noise
-s_max = GetParam(me,'s_max','value');
-s_min = GetParam(me,'s_min','value');
-s_res = GetParam(me,'s_res','value');
-wn = randn(samples,1);
-wn = wn - min(wn);
-wn = round(wn * s_res / max(wn)) * (s_max - s_min) / s_res + s_min;
-    
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function writeStimulus(filename, stimulus, stimrate, analysis_interval)
-% writes stimulus waveform to a mat file for later analysis
-[pn fn ext] = fileparts(filename);
-save([pn filesep fn '.mat'],...
-    'stimulus', 'stimrate', 'analysis_interval');
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%55
-function updateDisplay(obj, event)
-% this method displays data, using peekdata to acquire the latest
-% bit of data
-samp = get(obj,'TimerPeriod');
-sr = get(obj,'SampleRate'); % samp/sec
-d = peekdata(obj,samp*sr);
-if length(d) == samp*sr % short stuff is discarded
-    sr = 1000/sr; % ms/samp
-    t = 0:sr:(length(d)-1)*sr;
-    plotData(t,d);
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function varargout = plotData(time, data)
-% updates the scope with the latest bit of data
-global wc
-
-mode = GetParam('control.telegraph', 'mode');
-gain = GetParam('control.telegraph', 'gain');
-scope = getScope;
-if ~isempty(mode)
-    units = TelegraphReader('units',mean(data(:,mode)));
-else
-    units = 'V';
-end
-if ~isempty(gain)
-    gain = TelegraphReader('gain',mean(data(:,gain)));
-else
-    gain = 1;
-end
-lbl = get(scope,'YLabel');
-set(lbl,'String',['amplifier (' units ')']);
-% plot the data and average response
-index = wc.control.amplifier.Index;
-data = AutoGain(data(:,index), gain, units);
-Scope('scope',time, data);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function analyze(obj, event)
-% this method analyzes the data, using getdata to clear the latest
-% data from the buffer
-global wc
-window = [-1000 200];
-[data, time, abstime] = getdata(obj);
-index = wc.control.amplifier.Index;
-stim = Spool('stim','retrieve');
-samplerate = get(obj,'SampleRate');
-t_res = GetParam(me,'t_res','value');
-stimstart = get(wc.ao,'InitialTriggerTime');
-c = revcorr(data(:,index)', stim, samplerate,...
-    1000 / t_res, stimstart, abstime, window);
-s = [me '.analysis'];
-f = findobj('tag', s);
-if isempty(f) | ~ishandle(f)
-    f = figure('tag', s, 'numbertitle', 'off', 'name', s);
-end
-t = window(1):t_res:window(2);
-figure(f);
-d = get(f,'UserData');
-d = cat(1,d,c);
-a = mean(d,1);
-p = plot(t, [c; a]);
-xlabel('Time (ms)');
-set(f,'name',[s ' - ' num2str(size(d,1)) ' scans']);
-set(f,'UserData',d);
-Spool('stim','delete');
-
-startSweep;
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function setupScope();
-% sets up the scope properties
-scope = getScope;
-clearPlot;
-%set(scope, 'YLim', [-3 3]);
-set(scope, 'XLim', [0 1000]);
-set(scope, 'NextPlot', 'add');
-lbl = get(scope,'XLabel');
-set(lbl,'String','Time (ms)');
+% Spool('stim','init');
+% Spool('resp','init');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%55555
 function startSweep()
@@ -274,24 +154,196 @@ stop([wc.ai wc.ao]);
 flushdata(wc.ai);
 fn = get(wc.ai,'LogFileName');
 set(wc.ai,'LogFileName',NextDataFile(fn));    
-SetUIParam('scope','status','String',get(wc.ai,'logfilename'));
+SetUIParam('protocolcontrol','status','String',get(wc.ai,'logfilename'));
 queueStimulus;
 start([wc.ai wc.ao]);
 trigger([wc.ai wc.ao]);
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
-function scope = getScope()
-scope = GetUIHandle('scope','scope');
-if (isempty(scope) | ~ishandle(scope))
-    Scope('init');
-    scope = GetUIHandle('scope','scope');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%55
+function len = queueStimulus(ai, ao)
+% queues data in the ao object, returning the length of the sequence (in frames)
+% also writes the stimulus to disk if the ai is in disk-logging mode
+p       = GetParam(me,'load_me','value');       % is a sequence cached?
+if p
+    seqf    = GetParam(me,'sequence','value');
+    seq     = loadSequence(seqf);
+else
+    seq     = GetUIParam('protocolcontrol','status','UserData');
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
-function clearPlot()
-Scope('clear');
+% rescale sequence to voltages
+minV    = GetParam(me,'s_min','value');
+maxV    = GetParam(me,'s_max','value');
+seq     = (seq - min(min(seq)) * (maxV - minV) / max(max(seq)) + minV;
 
-%%%%%%%%%%%%%%%%%%%%%%%%5
-function showerr(obj, event)
-keyboard;
+[len,n] = size(seq);                    % length and number of signals
+nchan   = length(ao.Channel);           % number of channels
+c       = zeros(len,nchan);             % need to fill all the channels
+if n < nchan
+    c(:,1:n) = seq;
+elseif n > nchan
+    c = seq(:,1:nchan);
+else
+    c = seq;
+end
+putdata(ao,c);                          % here is where the data gets sent to the ao obj
+
+m = get(ai,'LoggingMode')
+if ~strcmpi('memory',m)
+    lf = get(ai,'Logfilename')
+    writeStimulus(lf,seq);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function seq = loadSequence(filename)
+% loads the sequence from a file, throwing errors when appropriate
+if ~exist(filename)
+    errordlg(['Sequence file does not exist']);
+    error('Unable to load sequence file');
+end
+[pn fn ext] = fileparts(filename)
+switch lower(ext)
+case '.mat'
+    seq = load(filename);
+    fn  = fieldnames(seq);      % load first field
+    seq = getfield(seq,fn);
+case '.m'
+    seq = feval(filename);
+otherwise
+    errordlg(['Invalid sequence file!']);
+    error('Unable to load sequence from file');
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function writeStimulus(filename, stimulus)
+% writes stimulus waveform to a mat file for later analysis
+frate = GetParam(me,'f_rate','value');
+[pn fn ext] = fileparts(filename);
+save([pn filesep fn '.mat'],...
+    'stimulus', 'frate');
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%55
+% function updateDisplay(obj, event)
+% % this method displays data, using peekdata to acquire the latest
+% % bit of data
+% samp = get(obj,'TimerPeriod');
+% sr = get(obj,'SampleRate'); % samp/sec
+% d = peekdata(obj,samp*sr);
+% if length(d) == samp*sr % short stuff is discarded
+%     sr = 1000/sr; % ms/samp
+%     t = 0:sr:(length(d)-1)*sr;
+%     plotData(t,d);
+% end
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% function varargout = plotData(time, data)
+% % updates the scope with the latest bit of data
+% global wc
+% 
+% mode = GetParam('control.telegraph', 'mode');
+% gain = GetParam('control.telegraph', 'gain');
+% scope = getScope;
+% if ~isempty(mode)
+%     units = TelegraphReader('units',mean(data(:,mode)));
+% else
+%     units = 'V';
+% end
+% if ~isempty(gain)
+%     gain = TelegraphReader('gain',mean(data(:,gain)));
+% else
+%     gain = 1;
+% end
+% lbl = get(scope,'YLabel');
+% set(lbl,'String',['amplifier (' units ')']);
+% % plot the data and average response
+% index = wc.control.amplifier.Index;
+% data = AutoGain(data(:,index), gain, units);
+% Scope('scope',time, data);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function analyze(obj, event)
+% this function is called when the full set of data has been acquired
+% it collects data from the buffer, plots it, and calls the analysis method
+% if there are additional repeats to run, it calls startSweep()
+global wc
+stop([obj wc.ao]);
+lfn       = get(obj,'LogFileName');
+[pn fn e] = fileparts(lfn);
+if ~strcmp('memory',lower(get(obj,'LoggingMode')))
+    save(fullfile(pn,[fn '-param']),'param');
+end
+plotResults(obj, wc.ao);
+r         = GetParam(me,'repeat','value');
+if r == 0 | r > str2num(fn)
+    startSweep;
+else
+    ClearAI(obj);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function plotResults(ai, ao)
+% this method plots and analyzes the data in the buffer
+in      = GetParam(me,'input','value');
+[data, time, abstime] = getdata(obj);
+axes(getScope)
+plot(time,data(:,in));
+% quick analysis using PCA_2D
+stim    = GetUIParam('protocolcontrol','status','UserData');
+srate   = get(ai,'SampleRate');     % sample rate
+frate   = get(ao,'SampleRate');     % LED frame rate
+y       = bindata(data(:,in),srate/frate,1);        % bin response to frame rate
+options = struct('correct','no');
+kern    = danlab_revcor(stim',y,10,frate,options);
+
+
+% window = [-1000 200];
+% 
+% index = wc.control.amplifier.Index;
+% stim = Spool('stim','retrieve');
+% samplerate = get(obj,'SampleRate');
+% t_res = GetParam(me,'t_res','value');
+% stimstart = get(wc.ao,'InitialTriggerTime');
+% c = revcorr(data(:,index)', stim, samplerate,...
+%     1000 / t_res, stimstart, abstime, window);
+% s = [me '.analysis'];
+% f = findobj('tag', s);
+% if isempty(f) | ~ishandle(f)
+%     f = figure('tag', s, 'numbertitle', 'off', 'name', s);
+% end
+% t = window(1):t_res:window(2);
+% figure(f);
+% d = get(f,'UserData');
+% d = cat(1,d,c);
+% a = mean(d,1);
+% p = plot(t, [c; a]);
+% xlabel('Time (ms)');
+% set(f,'name',[s ' - ' num2str(size(d,1)) ' scans']);
+% set(f,'UserData',d);
+% Spool('stim','delete');
+% 
+% startSweep;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
+function a = getScope()
+% retrieves the handle for the scope axes
+f       = findfig([me '.scope']);
+set(f,'position',[288 314 738 508],'name','scope','numbertitle','off');
+a       = get(f,'Children');
+if isempty(a)
+    a   = axes;
+    set(a,'NextPlot','ReplaceChildren')
+    set(a,'XTickMode','Auto','XGrid','On','YGrid','On','YLim',[-5 5])
+    xlabel('Time (ms)')
+    ylabel('amplifier (V)')
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function clearDAQ()
+% resets callbacks so we don't get recordings where we don't want'em
+global wc
+set(wc.ai,'SamplesAcquiredAction',{});
+set(wc.ai,'TimerAction',{});
+SetUIParam('wholecell','status','String',get(wc.ai,'Running'));
+set(wc.ai,'LoggingMode','Memory');
+set(wc.ai,'LogFileName',NextDataFile);
+set(wc.ai,'TriggerType','Manual')
