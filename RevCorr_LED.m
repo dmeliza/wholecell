034ffa3b39aa32322caabafa9fb45a2557546129
lab <>
2004-01-08 22:50:15 +0000

    fixed problems with synchronization, now correctly reverse correlates a single direct line from daq out to input.  for some reason, though it doesn't work when the sample rate of the ao is much lower than the sample rate of the ai

--- RevCorr_LED.m
+++ RevCorr_LED.m
@@ -21,9 +21,13 @@ function varargout = RevCorr_LED(varargin)
 % Details: The operation of this protocol is quite straightforward.  The user specifies
 %          a frame rate and an output sequence.  The sequence is loaded into the daq's
 %          analog output, and when the user clicks "Start" or "Record", the ai and ao
-%          objects are triggered simultaneously.  Empirically, synchronization is very
-%          tight between the input and output, so no additional signal is needed to
-%          align the stimulus waveform with the response.
+%          objects are triggered simultaneously.  Empirically, although the DAQ toolbox
+%          claims that this causes both objects to start simultaneously, this is not
+%          always the case, so we do need a synchronization signal to make sure that
+%          the stimulus and response are in frame.  This is done by feeding one of the
+%          LED signals into a separate input. The ao is reset to 0 before starting, so
+%          the data can be realigned based on when the sync signal transitions from 0
+%          to either the ON or the OFF state.
 %
 % $Id$
 
@@ -31,7 +35,7 @@ global wc
 
 if isobject(varargin{1})
     feval(varargin{3},varargin{1:2});
-    return;
+    return
 end
 
 if nargin > 0
@@ -103,19 +107,15 @@ global wc;
     p.s_min.fieldtype = 'value';
     p.s_min.units = 'V';
     p.s_min.value = 2.0;
-%     p.a_int = struct('description','Repeat Length','fieldtype','value',...
-%                       'value',50,'units','s');
     p.repeat = struct('description','Repeats (0=inf)','fieldtype','value',...
-                      'value',0.2);
+                      'value',1);
     p.f_rate = struct('description','Frame Rate','fieldtype','value',...
                      'units','Hz','value',20);
-%     p.d_rate = struct('description','Display Rate','fieldtype','value',...
-%                       'value',10,'units','Hz');
-    p.input.description = 'Amplifier Channel';
-    p.input.fieldtype = 'list';
-    p.input.choices = GetChannelList(wc.ai);
-    ic = get(wc.control.amplifier,'Index');
-    p.input.value = ic;
+    c = GetChannelList(wc.ai);
+    p.sync = struct('description','Sync Channel','fieldtype','list',...
+                    'choices',{c},'value',1);
+    p.input = struct('description','Amplifier Channel','fieldtype','list',...
+                     'choices',{c},'value',1);
     
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function setupHardware()
@@ -124,27 +124,21 @@ global wc
 %display = @updateDisplay;
 analyze = @analyze;
 srate   = get(wc.ai, 'SampleRate');               % sample rate of ai
-frate   = GetParam(me,'f_rate','value');           % frame rate of LED in Hz
-%d_rate  = GetParam(me,'d_rate','value');          % update rate, Hz
-len     = queueSequence(wc.ai, wc.ao);            % number of frames
-a_int   = sr * GetParam(me,'a_int','value');      % analysis interval, samples
+frate   = GetParam(me,'f_rate','value');          % frame rate of LED in Hz
+%d_rate  = GetParam(me,'d_rate','value');         % update rate, Hz
+len     = loadStimulus;                           % number of frames
+a_int   = (len + 4) / frate * srate;               % analysis interval, samples
+delay   = 2 / frate * srate;                      % 2 sample delay
 %update = fix(sr / u_rate); 
 set(wc.ai,'SamplesPerTrigger',a_int);
-%set(wc.ai,'TimerPeriod',1 / u_rate);
-%set(wc.ai,'TimerAction',{me,display})
+set(wc.ai,'TriggerDelayUnits','samples');
+set(wc.ai,'TriggerDelay',-delay);
 set(wc.ai,'SamplesAcquiredActionCount',a_int);
 set(wc.ai,'SamplesAcquiredAction',{me,analyze});
-set(wc.ai,'DataMissedAction',{me,'showerr'});
-%set(wc.ai,'UserData',update);
 set([wc.ai wc.ao],'TriggerType','Manual');
-set(wc.ai,'ManualTriggerHwOn','Trigger');
+set(wc.ai,'ManualTriggerHwOn','Start');
 
 set(wc.ao, 'SampleRate', frate);
-% set(wc.ao,'SamplesOutputFcnCount', sr * a_int * 19);
-% set(wc.ao,'SamplesOutputFcn',{me, requeue});
-
-% Spool('stim','init');
-% Spool('resp','init');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%55555
 function startSweep()
@@ -155,29 +149,33 @@ flushdata(wc.ai);
 fn = get(wc.ai,'LogFileName');
 set(wc.ai,'LogFileName',NextDataFile(fn));    
 SetUIParam('protocolcontrol','status','String',get(wc.ai,'logfilename'));
-queueStimulus;
+queueStimulus(wc.ai,wc.ao);
 start([wc.ai wc.ao]);
+pause(0.1);
 trigger([wc.ai wc.ao]);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function len = loadStimulus()
+% Loads the stimulus from the disk and stores it in the status UserData
+seqf    = GetParam(me,'sequence','value');
+seq     = loadSequence(seqf);
+SetUIParam('protocolcontrol','status','UserData',seq);
+len     = size(seq,1);
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%55
-function len = queueStimulus(ai, ao)
+function [] = queueStimulus(ai, ao)
 % queues data in the ao object, returning the length of the sequence (in frames)
 % also writes the stimulus to disk if the ai is in disk-logging mode
-p       = GetParam(me,'load_me','value');       % is a sequence cached?
-if p
-    seqf    = GetParam(me,'sequence','value');
-    seq     = loadSequence(seqf);
-else
-    seq     = GetUIParam('protocolcontrol','status','UserData');
-end
+seq     = GetUIParam('protocolcontrol','status','UserData');
 
 % rescale sequence to voltages
 minV    = GetParam(me,'s_min','value');
 maxV    = GetParam(me,'s_max','value');
-seq     = (seq - min(min(seq)) * (maxV - minV) / max(max(seq)) + minV;
+seq     = (seq - min(min(seq))) * (maxV - minV) / max(max(seq)) + minV;
 
 [len,n] = size(seq);                    % length and number of signals
 nchan   = length(ao.Channel);           % number of channels
+putsample(ao,zeros(1,nchan));           % reset output
 c       = zeros(len,nchan);             % need to fill all the channels
 if n < nchan
     c(:,1:n) = seq;
@@ -188,7 +186,7 @@ else
 end
 putdata(ao,c);                          % here is where the data gets sent to the ao obj
 
-m = get(ai,'LoggingMode')
+m = get(ai,'LoggingMode');
 if ~strcmpi('memory',m)
     lf = get(ai,'Logfilename')
     writeStimulus(lf,seq);
@@ -201,12 +199,12 @@ if ~exist(filename)
     errordlg(['Sequence file does not exist']);
     error('Unable to load sequence file');
 end
-[pn fn ext] = fileparts(filename)
+[pn fn ext] = fileparts(filename);
 switch lower(ext)
 case '.mat'
     seq = load(filename);
     fn  = fieldnames(seq);      % load first field
-    seq = getfield(seq,fn);
+    seq = getfield(seq,fn{1});
 case '.m'
     seq = feval(filename);
 otherwise
@@ -221,44 +219,6 @@ frate = GetParam(me,'f_rate','value');
 [pn fn ext] = fileparts(filename);
 save([pn filesep fn '.mat'],...
     'stimulus', 'frate');
-
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%55
-% function updateDisplay(obj, event)
-% % this method displays data, using peekdata to acquire the latest
-% % bit of data
-% samp = get(obj,'TimerPeriod');
-% sr = get(obj,'SampleRate'); % samp/sec
-% d = peekdata(obj,samp*sr);
-% if length(d) == samp*sr % short stuff is discarded
-%     sr = 1000/sr; % ms/samp
-%     t = 0:sr:(length(d)-1)*sr;
-%     plotData(t,d);
-% end
-
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% function varargout = plotData(time, data)
-% % updates the scope with the latest bit of data
-% global wc
-% 
-% mode = GetParam('control.telegraph', 'mode');
-% gain = GetParam('control.telegraph', 'gain');
-% scope = getScope;
-% if ~isempty(mode)
-%     units = TelegraphReader('units',mean(data(:,mode)));
-% else
-%     units = 'V';
-% end
-% if ~isempty(gain)
-%     gain = TelegraphReader('gain',mean(data(:,gain)));
-% else
-%     gain = 1;
-% end
-% lbl = get(scope,'YLabel');
-% set(lbl,'String',['amplifier (' units ')']);
-% % plot the data and average response
-% index = wc.control.amplifier.Index;
-% data = AutoGain(data(:,index), gain, units);
-% Scope('scope',time, data);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function analyze(obj, event)
@@ -284,44 +244,25 @@ end
 function plotResults(ai, ao)
 % this method plots and analyzes the data in the buffer
 in      = GetParam(me,'input','value');
-[data, time, abstime] = getdata(obj);
+sync    = GetParam(me,'sync','value');
+[data, time, abstime] = getdata(ai);
 axes(getScope)
-plot(time,data(:,in));
-% quick analysis using PCA_2D
+% align input and output timings
 stim    = GetUIParam('protocolcontrol','status','UserData');
 srate   = get(ai,'SampleRate');     % sample rate
 frate   = get(ao,'SampleRate');     % LED frame rate
-y       = bindata(data(:,in),srate/frate,1);        % bin response to frame rate
-options = struct('correct','no');
-kern    = danlab_revcor(stim',y,10,frate,options);
+ini     = mean(data(1:100,sync));
+ind     = find(data(:,sync)<ini*3); % indices of data near minimum value
+ind     = max(ind)+1;
+data    = data(ind:end,in);
+time    = time(ind:end);
+plot(time,data);
 
-
-% window = [-1000 200];
-% 
-% index = wc.control.amplifier.Index;
-% stim = Spool('stim','retrieve');
-% samplerate = get(obj,'SampleRate');
-% t_res = GetParam(me,'t_res','value');
-% stimstart = get(wc.ao,'InitialTriggerTime');
-% c = revcorr(data(:,index)', stim, samplerate,...
-%     1000 / t_res, stimstart, abstime, window);
-% s = [me '.analysis'];
-% f = findobj('tag', s);
-% if isempty(f) | ~ishandle(f)
-%     f = figure('tag', s, 'numbertitle', 'off', 'name', s);
-% end
-% t = window(1):t_res:window(2);
-% figure(f);
-% d = get(f,'UserData');
-% d = cat(1,d,c);
-% a = mean(d,1);
-% p = plot(t, [c; a]);
-% xlabel('Time (ms)');
-% set(f,'name',[s ' - ' num2str(size(d,1)) ' scans']);
-% set(f,'UserData',d);
-% Spool('stim','delete');
-% 
-% startSweep;
+% quick analysis using danlab_revcor
+y       = bindata(data,srate/frate,1);        % bin response to frame rate
+options = struct('correct','no','display','yes');
+keyboard
+kern    = danlab_revcor(stim,y(1:length(stim)),10,frate,options);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
 function a = getScope()
