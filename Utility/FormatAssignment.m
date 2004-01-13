b05e3575fd8653d057277538c21d4199b6b8e41c
meliza <>
2004-01-13 02:26:03 +0000

    cleanup and documentation

--- Utility/FormatAssignment.m
+++ Utility/FormatAssignment.m
@@ -1,21 +1,24 @@
 function sfp = FormatAssignment(property, value)
-% When generating assignment string intended to be eval'd
+% When generating assignment strings intended to be eval'd
 % we have to know what the format of the data is so that
-% we can make the correct string.
+% we can make the correct string.  This function is used by
+% SetParam.  It can't really deal with anything but strings
+% and numeric values, as I have no desire to convert cells or
+% structs to string equivalents, and it's pretty easy to just
+% assign that value directly.
 % 
-% OUT = FormatValue(value)
+% USAGE: str = FormatValue(property, value)
 %
+% property - the field identifier
+% value    - the value to assign to the field
+% str      - the string that can be eval'd to assign the value to the field
 %
 % $Id$
-% we have to escape string assignments
+
 if (ischar(value))
     sfp = sprintf('%s = ''%s'';', property, value);
-elseif (isnumeric(value)) % assignment of values
+elseif (isnumeric(value))                   
     sfp = sprintf('%s = %d;', property, value);
-else % structs, cell arrays, and pointers, we need to know the variable name
-%     if (nargin > 1)
-%         sfp = varargin{1};
-%     else
-        disp('Can''t deal with this data structure');
-%     end
+else 
+    error('FormatAssignment can''t deal with this data structure');
 end
--- Utility/GetChannelList.m
+++ Utility/GetChannelList.m
@@ -1,6 +1,15 @@
 function out = GetChannelList(daqdevice)
 % returns a cell array of pretty-printed information about
-% channels in a daqdevice
+% channels in a daqdevice.
+%
+% USAGE: out = GetChannelList(daqdevice)
+%
+% daqdevice     - a daqdevice object (with the Channel property)
+% out           - a cell array of strings describing the channels in the daqdevice
+%
+% $Id$
+%
+
 out = '';
 c = get(daqdevice.Channel,{'HwChannel','ChannelName','Units'});
 for i=1:size(c,1);
