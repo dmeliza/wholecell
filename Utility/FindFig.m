54f57527e91283f16c342b34dc58bffe9e1d6903
meliza <>
2004-01-13 02:18:00 +0000

    code and docs cleanup, restricted search to figures

--- Utility/FindFig.m
+++ Utility/FindFig.m
@@ -1,16 +1,22 @@
 function handle = FindFig(tag)
 %
 % Opens a figure window with a given tag.  Or if one already exists,
-% sets it as current and returns the handle
+% sets it as current and returns the handle.
+%
+% Usage:  handle = FindFig(tag)
+%
+% tag - a string that matches the figure's tag (or will be the new figure's tag)
+%
+% handle - the matlab GUI handle that identifies the figure
 %
 % $Id$
 
 error(nargchk(1,1,nargin))
 
-handle = findobj('tag',tag);
+handle      = findobj('tag',tag,'type','figure');
 if ishandle(handle)
     figure(handle);
 else
-    handle = figure;
+    handle  = figure;
     set(gcf, 'tag', tag);
 end
