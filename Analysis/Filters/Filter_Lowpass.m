d700a2fb26a0a0d541da208247ed7133af49a4df
meliza <>
2004-01-27 20:01:18 +0000

    initial rev

---
+++ Analysis/Filters/Filter_Lowpass.m
@@ -0,0 +1,41 @@
+function data = Filter_Lowpass(action, data, parameters)
+% Filters data using an n-pole butterworth lowpass filter.
+% Uses FiltFilt to avoid phase offsets.
+%
+% parameters:
+%    .pass  - passband, in Hz
+%    .order - order of the filter, default = 3
+%
+% undefined results if pass > Fs of data
+%
+% $Id$
+error(nargchk(1,3,nargin))
+
+switch lower(action)
+case 'params'
+    prompt = {'Passband (Hz)','Order'};
+    if nargin > 1
+        def = struct2cell(data);
+    else
+        def = {'1','3'};
+    end
+    title   = 'Values for LowPass Filter (ignored)';
+    answer  = inputdlg(prompt,title,1,def);
+    data    = struct('pass',abs(str2num(answer{1})),'order',abs(fix(str2num(answer{2}))));
+case 'describe'
+    data    = sprintf('LowPass Filter (%d Hz, order %d)', data.pass, data.order);
+case 'filter'
+    if isstruct(data)
+        if ~isfield(parameters,'order')
+            parameters.order = 3;
+        end
+        for i = 1:length(data);
+            Fs      = data(i).t_rate;
+            Wn      = parameters.pass/(Fs/2);
+            [b,a]   = butter(parameters.order,Wn);
+            data(i).data = filtfilt(b,a,data(i).data);  % modify data in place
+        end
+    end
+otherwise
+    error(['Action ' action ' is not supported.']);
+end
