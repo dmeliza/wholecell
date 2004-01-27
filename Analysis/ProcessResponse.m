08a86b4508b1efb0e29018fe9ca16950dce3c845
meliza <>
2004-01-27 19:44:53 +0000

    mostly functional at this point

--- Analysis/ProcessResponse.m
+++ Analysis/ProcessResponse.m
@@ -13,7 +13,7 @@ function [] = ProcessResponse(signals)
 %
 % [] = ProcessResponse(signals)
 %
-% signals - an r1 structure [array]
+% signals - an r1 structure [array], or the filename of an r1 file
 %
 % zooming is linked between axes
 %
@@ -21,11 +21,13 @@ function [] = ProcessResponse(signals)
 
 error(nargchk(0,1,nargin))
 
-filters = {'highpass','bandpass','lowpass','bin','framebin','spikes'};
+filters = {'highpass','bandpass','lowpass','bin','framebin','spikes','null'};
 
 initFigure;
 initValues(filters);
-% loadSignals(signals);
+if nargin > 0
+    loadSignals(signals);
+end
 
 function [] = initFigure()
 % creates the figure window and populates it with GUI objects
@@ -34,13 +36,14 @@ BG = [1 1 1];
 f = OpenFigure(me,'position',[360   343   700   600],...
     'color',BG,'menubar','none');
 movegui(f,'northwest')
+set(f,'WindowButtonDownFcn',cb.clickaxes);
 
 % first axes - raw signals
 h  = InitUIObject(me, 'input', 'axes', 'units','pixels','position',[50 400 620 180],...
-    'nextplot','replacechildren','Box','On');%,'XTick',[],'Ytick',[],'Box','On');
+    'nextplot','replacechildren','Box','On');% 'ButtonDownFcn',cb.clickaxes);
 % second axes - processed signals
-h  = InitUIObject(me, 'input', 'axes', 'units','pixels','position',[50 50 620 180],...
-    'nextplot','replacechildren','Box','On');
+h  = InitUIObject(me, 'output', 'axes', 'units','pixels','position',[50 50 620 180],...
+    'nextplot','replacechildren','Box','On');% 'ButtonDownFcn', cb.clickaxes);
 % control frame
 h = uicontrol(gcf, 'style', 'frame','backgroundcolor',BG,'position',[50 250 620 120]);
 h = uicontrol(gcf, 'style', 'text', 'position',[60 350 120 15],'String','Filters',...
@@ -63,23 +66,86 @@ h = InitUIControl(me, 'filterdown', 'style', 'pushbutton', 'position', [380 280 
 h = InitUIControl(me,'filterdesc','style','text','backgroundcolor',BG,...
     'position', [420 260 160 100], 'HorizontalAlignment', 'left', 'String', '');
 % buttons
-h = InitUIControl(me, 'loadchain', 'style', 'pushbutton', 'position', [590 325 70 25],...
+h = InitUIControl(me, 'applychain', 'style', 'pushbutton', 'position', [590 340 70 25],...
+    'String','Apply Filters','callback',cb.applychain);
+h = InitUIControl(me, 'loadchain', 'style', 'pushbutton', 'position', [590 313 70 25],...
     'String','Load Filters','callback',cb.loadchain);
-h = InitUIControl(me, 'savechain', 'style', 'pushbutton', 'position', [590 295 70 25],...
+h = InitUIControl(me, 'savechain', 'style', 'pushbutton', 'position', [590 286 70 25],...
     'String','Save Filters','callback',cb.savechain);
-h = InitUIControl(me, 'savesignal', 'style', 'pushbutton', 'position', [590 265 70 25],...
+h = InitUIControl(me, 'savesignal', 'style', 'pushbutton', 'position', [590 259 70 25],...
     'String', 'Save Signal', 'callback', cb.savesignal);
 
 function [] = initValues(filters)
 % initializes the lists to their default values
 SetUIParam(me,'filters','String',filters);
 
+function [] = loadSignals(signals)
+% loads data into the input window
+if isstr(signals)
+    if exist(signals)
+        signals = CombineFiles('r1',signals);
+    else
+        error(['File ' signals ' does not exist!']);
+    end
+end
+if ~isstruct(signals)
+    error('Signal argument must be an r1 structure or an r1 file name');
+end
+a = GetUIHandle(me,'input');            % axes handle
+set(a,'UserData',signals);
+plotSignals(a,signals);
+%zoom(gcf,'on');
+
+
 function param = getParameters(filtername, parameters)
 % runs a dialog to allow the user to select parameters for the filter
+str = sprintf('Filter_%s',filtername);      % the name of the mfile to execute
+if ~exist(str)
+    SetUIParam(me,'filterdesc','String','Filter not implemented');
+    param = [];
+else
+    if nargin == 3
+        param = feval(str, 'params', parameters);
+    else
+        param = feval(str, 'params');
+    end
+end
 
 function desc = getFilterDescription(filtername, parameters)
 % returns a friendly string description of the filter's effects
-desc = filtername;
+str  = sprintf('Filter_%s',filtername);
+if ~exist(str)
+    desc = 'Filter not implemented';
+else
+    desc = feval(str, 'describe', parameters);
+end
+
+function [] = plotSignals(axesHandle, signals)
+axes(axesHandle)
+for i = 1:length(signals)
+    len = length(signals(i).data);
+    t   = linspace(0,len/signals(i).t_rate,len);
+    plot(t,signals(i).data)
+end
+ylabel(signals(1).y_unit)
+
+function signals = executeChain(signals);
+% executes the filter chain on an r1 structure, returning another r1 structure
+filtername = GetUIParam(me,'activefilters','String');
+if ~isempty(filtername)
+    param  = GetUIParam(me,'activefilters','UserData');
+    for i = 1:length(filtername)
+        str = sprintf('Filter_%s',filtername{i});
+        if ~exist(str)
+            warning(['Filter ' filtername{i} ' has not been implemented; skipped.']);
+        else
+            signals = feval(str,'filter',signals,param{i});
+        end
+    end
+else
+    signals = [];
+end
+    
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Callbacks:
@@ -180,12 +246,50 @@ if ~isnumeric(fn)
 end
 
 function [] = savesignal(obj, event)
+% callback for save button
+[fn pn] = uiputfile('*.r1');
+if ~isnumeric(fn)
+    in  = GetUIParam(me,'input','UserData');
+    out = executeChain(in);
+    if ~isempty(out)
+        save(fullfile(pn,fn),'out','-mat');
+    else
+        warning('Filter chain did not return any results!');
+    end
+end
+
+function [] = applychain(obj, event)
+% callback for apply button
+in  = GetUIParam(me,'input','UserData');
+a   = GetUIHandle(me,'output');
+axes(a);
+cla;
+out = executeChain(in);
+if ~isempty(out)
+    plotSignals(a,out);
+end
+
+function [] = clickaxes(obj, event)
+% this function is used to synchronize zoom operations between axes
+zoom(gcbf,'down')           % execute matlab's zoom function
+obj  = get(gcbf, 'currentaxes');
+xlim = get(obj,'XLim');
+ylim = get(obj,'YLim');
+tag  = get(obj,'Tag');
+if strcmpi(tag,'input')
+    otag = 'output';
+else
+    otag = 'input';
+end
+SetUIParam(me,otag,'XLim',xlim);
+SetUIParam(me,otag,'YLim',ylim);
+
 
 function out = getCallbacks()
 % returns a structure with function handles to functions in this mfile
 % no introspection in matlab so we have to do this by hand
 fns = {'addfilter','removefilter','activefilters','filterup','filterdown', 'savechain',...
-        'savesignal','loadchain'};
+        'savesignal','loadchain','applychain','clickaxes'};
 out = [];
 for i = 1:length(fns)
     sf = sprintf('out.%s = @%s;',fns{i},fns{i});
