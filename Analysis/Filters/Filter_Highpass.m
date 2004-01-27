ded393105f182dc312dbae7de710e10fb16bee17
meliza <>
2004-01-27 19:57:48 +0000

    first revision

---
+++ Analysis/Filters/Filter_Highpass.m
@@ -0,0 +1,49 @@
+function data = Filter_Highpass(action, data, parameters)
+% Filters data using an n-pole butterworth highpass filter.
+% Uses FiltFilt to avoid phase offsets.
+%
+% parameters:
+%    .pass  - passband, in Hz
+%    .order - order of the filter, default = 3
+%
+% undefined results if pass > Fs of data
+%
+% $Id$
+error(nargchk(1,3,nargin))
+
+switch lower(action)
+case 'params'
+    prompt = {'Passband (Hz)','Order'};
+    if nargin > 1
+        def = struct2cell(data);
+    else
+        def = {'1','3'};
+    end
+    title   = 'Values for HighPass Filter (ignored)';
+    answer  = inputdlg(prompt,title,1,def);
+    data    = struct('pass',abs(str2num(answer{1})),'order',abs(fix(str2num(answer{2}))));
+case 'describe'
+    data    = sprintf('HighPass Filter (%d Hz, order %d)', data.pass, data.order);
+case 'filter'
+    if isstruct(data)
+        if ~isfield(parameters,'order')
+            parameters.order = 3;
+        end
+        for i = 1:length(data);
+            Fs      = data(i).t_rate;
+            Wn      = parameters.pass/(Fs/2);
+            [b,a]   = butter(parameters.order,Wn,'high');
+            data(i).data = filtfilt(b,a,data(i).data);  % modify data in place
+        end
+    end
+otherwise
+    error(['Action ' action ' is not supported.']);
+end
+
+% error(nargchk(3,4,nargin))
+% if nargin < 4
+%     order   = 3;
+% end
+% Wn          = pass/(Fs/2);
+% [b,a]       = butter(order,Wn,'high');
+% data        = filtfilt(b,a,data);
