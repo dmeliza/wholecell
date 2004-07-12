3ffd4849b16db39a8d0fc45e21e832f7efd73c54
meliza <>
2004-07-12 21:08:23 +0000

    added support for independent ltp and ltd

--- Analysis/Figures/STDPWindow.m
+++ Analysis/Figures/STDPWindow.m
@@ -4,26 +4,53 @@ function [] = STDPWindow(csvfile)
 % If this is coming from prism we have to manipulate the post-induction
 % values into a single column
 %
+% 1.3: uses csv file with the following fields:
+%    rat/cell   pre   post  delay-onset   delay-peak
+% 
 % $Id$
 
 WIDTH   = 70;
-YLIM    = [0 2];
+YLIM    = [0 2.2];
 SZ      = [3.5 2.9];
+mode    = 'add';
+LTD_WIN = [-60 -1];
+LTP_WIN = [1 20];
 
-z   = csvread(csvfile);
+z   = csvread(csvfile,0,1);
 s   = size(z,2);
-if s > 2
-    z(:,2) = sum(z(:,2:end),2);
+% if s > 2
+%     z(:,2) = sum(z(:,2:end),2);
+% end
+pre     = z(:,1);
+post    = z(:,2);
+delay_o   = z(:,3);
+delay_p   = z(:,4);
+delay     = delay_p;
+switch mode
+    case 'add'
+        STDP    = (post - pre) ./ pre + 1
+    otherwise
+        STDP    = post ./ pre
 end
+
+% compute significance over windows
+ltp_ind = find(delay >= LTP_WIN(1) & delay <= LTP_WIN(2));
+ltd_ind = find(delay >= LTD_WIN(1) & delay <= LTD_WIN(2));
+ltp_p   = signrank(pre(ltp_ind),post(ltp_ind))
+ltd_p   = signrank(pre(ltd_ind),post(ltd_ind))
+
 f   = figure;
 set(f,'color',[1 1 1]);
 ResizeFigure(f,SZ);
 
+
+
 a   = axes;
-h   = plot(z(:,1),z(:,2),'ko');
+h   = plot(delay,STDP,'ko');
 %YLIM = [0,max(z(:,2))];
 set(a,'XLim',[-WIDTH WIDTH],'YLim',YLIM)
 hline(1)
 vline(0)
 xlabel('Pre/Postsynaptic Time Interval (ms)')
 ylabel('Normalized EPSC Amplitude')
+text(WIDTH * 0.6, YLIM(2) * 0.9, sprintf('(n = %d)',length(STDP)));
