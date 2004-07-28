2b8bfe9c737f1e7e7a63319714ab59b0e24e22de
meliza <>
2004-07-28 17:34:49 +0000

    old code removed

--- Analysis/Figures/CompareSpatialRF.m
+++ Analysis/Figures/CompareSpatialRF.m
@@ -30,75 +30,6 @@ else
         pst_cm, pst_cm_err(1), pst_cm_err(2), sig);
 end
 
-% types   = [exist(pre) exist(post)];
-% if all(types==7)
-%     [A,t,u] = loadFiles(pre);
-%     B       = loadFiles(post);
-%     a       = combineEpisodes(A);
-%     b       = combineEpisodes(B);
-% else
-%     A   = load(pre);
-%     B   = load(post);
-%     win     = SPATIALRF_WIN(1):SPATIALRF_WIN(2);
-%     t   = double(A.time(win,:)) * 1000 - 200;
-%     a   = double(A.data(win,:));
-%     b   = double(B.data(win,:));
-%     u   = A.units;
-% end
-% 
-% Fs  = mean(diff(t));
-% abase   = mean(mean(a(1:200,:)));
-% bbase   = mean(mean(b(1:200,:)));
-% a   = a - abase;
-% b   = b - bbase;
-% 
-% if nargin > 3
-%     i       = find(peak <= t);
-%     i       = i(1);
-% elseif nargin > 2
-%     [mx i]  = max(abs(b(:,induction)));
-%     fprintf('Max at %d, %3.2f ms\n', induction, t(i));    
-% else
-%     [mx i]  = max(abs(b));
-%     [mx j]  = max(mx);
-%     i       = i(j);
-%     fprintf('Max at %d, %3.2f ms\n', j, t(i));
-% end
-% 
-% w       = fix(RFWIN/Fs);
-% I       = (-w:w) + i;
-% arf     = a(I,:);
-% brf     = b(I,:);
-% 
-% 
-% pre_rf     = mean(arf,1);
-% post_rf    = mean(brf,1);
-% switch lower(u)
-%     case {'pa','na'}
-%         pre_rf = -pre_rf;
-%         post_rf = -post_rf;
-% end
-% 
-% 
-% % compute error bars
-% if iscell(A)
-% %     [pre_SD, pre_sem, pre_err]       = computeError(A,I,abase);
-% %     [post_SD, post_sem, post_err]    = computeError(B,I,bbase);
-% %     [precm,precm_err]                = centroid(pre_rf, pre_err);
-% %     [postcm,postcm_err]              = centroid(post_rf, post_err);
-%     pre_values                          = computeValues(A,I,abase);
-%     post_values                         = computeValues(B,I,bbase);
-% %    [precm,precm_err]                  = centroid
-%     keyboard
-%     fprintf('%3.2f +/- %3.2f (pre); %3.2f +/- %3.2f (post)\n',...
-%         precm, precm_err, postcm, postcm_err)
-% else
-%     precm   = centroid(pre_rf);
-%     postcm  = centroid(post_rf);
-%     fprintf('%3.2f (pre); %3.2f (post)\n',...
-%         precm, postcm)
-% end
-
 
 if nargout > 0
     return
@@ -116,12 +47,7 @@ else
     pre_cm   = interp1(1:length(X),X,pre_cm);
     pst_cm  = interp1(1:length(X),X,pst_cm);
 end
-% if iscell(A)
-%     p(1:2) = errorbar(X,pre_rf, pre_err, 'k');
-%     p(3:4) = errorbar(X,pst_rf, post_err, 'r');
-% else
-%     p       = plot(X,pre_rf,'k',X,post_rf,'r');
-% end
+
 pre_rf_err  = diff(pre_rf_err) ./ 2;
 pst_rf_err  = diff(pst_rf_err) ./ 2;
 p(1:2) = errorbar(X,pre_rf, pre_rf_err,'k');
@@ -144,47 +70,3 @@ if nargin > 2
     h   = plot(X(induction), mx * 1.4, 'kv');
     set(h,'MarkerFaceColor',[0 0 0])
 end
-
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% function [d,t,u] = loadFiles(directory)
-% global SPATIALRF_WIN
-% win     = SPATIALRF_WIN(1):SPATIALRF_WIN(2);
-% wd      = cd(directory);
-% dd      = dir('*.r0');
-% fls     = {dd.name};
-% for i = 1:length(fls)
-%     [pn fn ext] = fileparts(fls{i});
-%     seqfile     = fullfile(pn,[fn '.txt']);
-%     r0          = load('-mat',fls{i});
-%     D           = double(r0.r0.data(win,:,1));
-%     if exist(seqfile)
-%         S       = load('-ascii',seqfile);
-%         d{i}    = D(:,S,:);
-%     else
-%         d{i}    = D;
-%     end
-% end
-% t       = double(r0.r0.time(win,:)) * 1000 - 200;
-% u       = r0.r0.y_unit{1};
-% cd(wd)
-% 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% function m  = combineEpisodes(in)
-% % turns a cell array into a double array by averaging along dim 2
-% for i = 1:length(in)
-%     m(:,i)  = mean(in{i},2);
-% end
-% 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
-% function [tc] = computeValues(in, win, base)
-% % computes the 95% Cl of the mean value in the window
-% for i = 1:length(in)
-%     d       = in{i};
-%     d       = d(win,:) - base;
-%     tc{i}   = mean(d,1);                    % time course of mean
-% %     tval    = tinv(0.975,length(tc)-1);
-% %     SD(i)   = std(tc);
-% %     SEM(i)  = SD(i) / sqrt(length(tc));
-% %     CL(i)   = SEM(i) * tval;
-% end
-    
