cdbb07486a8827997fb084572a72aa570a1e3067
meliza <>
2004-09-06 23:38:40 +0000

    added plots of spatial spread of LTP/LTD

--- Analysis/Figures/CombineSpatialRF.m
+++ Analysis/Figures/CombineSpatialRF.m
@@ -49,23 +49,129 @@ STDP    = (pst_rf(i) - pre_rf(i))./pre_rf(i); % relative change in induced bar r
 isLTP   = t_spike >= t_peak;
 iscenter= (induction - peak) == 0;
 
-% Figure 1: Plot shift in RF as a function of LTP/LTD and center/surround
-plotStats(shift, isLTP, iscenter, NAMES, 'Shift in RF Center');
+% % Figure 1: Plot shift in RF as a function of LTP/LTD and center/surround
+% fprintf('\nRF Center:\n');
+% plotStats(shift, isLTP, iscenter, NAMES, 'Shift in RF Center');
+% 
+% % Figure 2: Compute width of RFs before and after induction
+% % this is somewhat ill-defined for a four-point RF, but it's basically:
+% % sigma = sqrt(sum((x-mu)^2 * y)/sum(y))
+% pre_sigma = getSigma(pre_rf, pre_cm);
+% pst_sigma = getSigma(pst_rf, pst_cm);
+% dff_sigma = pst_sigma - pre_sigma;      % increase or decrease in width
+% fprintf('\nRF Width:\n');
+% plotStats(dff_sigma, isLTP, iscenter, NAMES, 'Shift in RF Width');
 
-% Figure 2: Compute width of RFs before and after induction
-% this is somewhat ill-defined for a four-point RF, but it's basically:
-% sigma = sqrt(sum((x-mu)^2 * y)/sum(y))
-pre_sigma = getSigma(pre_rf, pre_cm);
-pst_sigma = getSigma(pst_rf, pst_cm);
-dff_sigma = pst_sigma - pre_sigma;      % increase or decrease in width
-plotStats(dff_sigma, isLTP, iscenter, NAMES, 'Shift in RF Width');
+% Figure 3: Plot spread of LTP/LTD as a function of distance. We can do
+% this a bunch of ways.  The most naive is to divide the data into LTP vs
+% LTD.  We'll plot this in one graph.  The other is to start distinguishing
+% between induction in the center and the surround, and for the latter
+% case, whether the noninduced bar is closer to the center (positive) or to the
+% surround (negative x).
+fprintf('\nRF Spread:\n')
+% initialize variables
+offset_offset = size(pre_rf,2);        % have to add this to make indices work
+LTP             = cell(1,offset_offset);
+LTD             = cell(1,offset_offset);
+LTP_cent        = cell(1,offset_offset);
+LTD_cent        = cell(1,offset_offset);
+LTP_surr        = cell(1,offset_offset * 2 - 1);
+LTD_surr        = cell(1,offset_offset * 2 - 1);
+% Cycle through the data and assign things to categories.
+for i = 1:trials
+    % each RF should be normalized against the strongest response
+    % (pre-induction)
+    pre     = pre_rf(i,:) ./ max(pre_rf(i,:));
+    pst     = pst_rf(i,:) ./ max(pre_rf(i,:));
+    fprintf('%s (%d, %d): ', files{i,1}, induction(i), peak(i));
+    for j = 1:size(pre_rf, 2)
+        change  = pst(j) - pre(j);
+        if ~isinf(change) & ~isnan(change)     % filter out div/0
+            x1      = abs(induction(i) - j);   % relative distance to induced
+            x2      = abs(peak(i) - j);        % relative distance to peak
+            sgn     = (x2 >= x1) * -1 + (x2 < x1);
+            offset  = x1 * sgn;
+            fprintf('%d->%d ', j, offset);
+            % sort by LTP vs LTD
+            o       = abs(x1) + 1;
+            if isLTP(i)
+                LTP{o}  = cat(1,LTP{o}, change);
+            else
+                LTD{o}  = cat(1,LTD{o}, change);
+            end
+            % sort by center vs surround and LTP vs LTD
+            if x1 == x2                     % true if induction bar is the strongest resp
+                o   = abs(offset) + 1;
+                if isLTP(i)
+                    LTP_cent{o}    = cat(1,LTP_cent{o},change);
+                else
+                    LTD_cent{o}    = cat(1,LTD_cent{o},change);
+                end
+            else
+                o       = offset + offset_offset;
+                if isLTP(i)
+                    LTP_surr{o}    = cat(1,LTP_surr{o},change);
+                else
+                    LTD_surr{o}    = cat(1,LTD_surr{o},change);
+                end
+            end
+        end
+    end
+    fprintf('\n')
+end
+% Plot 1: LTP and LTD in two graphs
+f   = figure;
+SZ  = [3.0 2.9];
+ResizeFigure(f,SZ)
 
+subplot(2,3,1);hold on;
+plotCell(LTP)
+
+subplot(2,3,4);hold on;
+plotCell(LTD)
+
+subplot(2,3,2);hold on;
+plotCell(LTP_cent)
+
+subplot(2,3,5);hold on;
+plotCell(LTD_cent)
+
+subplot(2,3,3);hold on;
+plotCell(LTP_surr,(1:length(LTP_surr))-offset_offset)
+
+subplot(2,3,6);hold on;
+plotCell(LTD_surr,(1:length(LTD_surr))-offset_offset)
+
+function [] = plotCell(y, x)
+% plots the mean and standard error of each array in a cell array along a
+% single axis.  The cell should be a row or column vector.  If x is not
+% supplied, the values 0:length(y)-1 are used.
+if nargin < 2
+    x   = 0:length(y)-1;
+end
+Z   = [];
+G   = [];
+for i = 1:length(y)
+    yy      = y{i};
+    Z       = cat(1,Z,yy);
+    G       = cat(1,G,repmat(x(i),length(yy),1));
+    Y(i)    = mean(yy);
+    Y_E(i)  = std(yy)/sqrt(length(yy));
+end
+
+h   = errorbar(x,Y,Y_E);
+%h    = plot(x,Y);
+set(h,'Color',[0 0 0],'LineWidth',2);
+%h    = plot(G,Z, 'ko');
+%set(h,'Color',[0.7 0.7 0.7],'MarkerFaceColor',[0.7 0.7 0.7],'MarkerSize',4);
+set(gca,'XLim',[x(1)-0.2 x(end)+0.2],'Box','On','XTick',x)
+hline(0)
+    
 
 function [] = plotStats(x, isLTP, iscenter, NAMES, y_label)
 % Generates a figure window displaying the results of a statistic, broken
 % down by LTP/LTD and center/surround
 SZ      = [3.0 2.9];
-XLIM    = [-2 2];
 
 f       = figure;
 set(f,'Color',[1 1 1])
