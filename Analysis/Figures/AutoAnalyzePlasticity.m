b60c9ccd0810d2676d81b53816527d2dc34e4dcd
meliza <>
2004-11-20 01:08:38 +0000

    added support for local control file

--- Analysis/Figures/AutoAnalyzePlasticity.m
+++ Analysis/Figures/AutoAnalyzePlasticity.m
@@ -10,6 +10,14 @@ function [] = AutoAnalyzePlasticity(fid)
 % been determined, the script will run a subscript to measure the amplitude
 % of the responses pre and post, and to determine the spike timing.
 %
+% This mfile will attempt to load a local control file (auto.mat) for help
+% in determining what to do with difficult directories.  This matfile
+% should contain the following structure:
+%   .ignore     - if 1, the whole directory is skipped
+%   .dirs       - 3x1 array with the INDICES of the dirs to use
+%   .t_pre      - Nx2 array of times specifying the onset and peak latency
+%   .t_post     - as with t_pre, but for post-induction
+%
 % $Id$
 
 % global parameters; empty if uninitialized
@@ -27,25 +35,52 @@ ANALYSIS_FN_RESP    = 'AutoAnalyzeResponse';
 ANALYSIS_FN_SPIKE   = 'AutoAnalyzeSpikeTiming';
 DAQ2MAT_MODE    = 'stack';
 DAQ2MAT_CHAN    = 1;
+LOCAL_CONTROL   = 'auto.mat';
+
+% load the control file, if there is one
+if exist(LOCAL_CONTROL) ~= 0
+    control     = load(LOCAL_CONTROL);
+    fprintf(fid, '[%s] - loaded control file\n', LOCAL_CONTROL);
+else
+    control     = struct([]);
+end
+
+% check for ignore flag
+if isfield(control,'ignore')
+    if getfield(control,'ignore')
+        fprintf(fid,'[%s] - Experiment ignored.\n', LOCAL_CONTROL);
+        return
+    end
+end
 
 % load subdirectories
 start_dir   = pwd;
 dd  = GetSubdirectories(FOLDER_SELECT);
 
 len = length(dd);
-fprintf(fid,'Subdirectories: %d', len)
+fprintf(fid,'Subdirectories: %d ', len)
 
 if len < 3
     fprintf(fid,'\n(experiment ignored)\n');
     return
 else
-    fprintf(fid,'\n');
     if len > 3
-        fprintf(fid,'(experiment ignored)\n');
-        return
-        % figure out which directories to use
+        if isfield(control,'dirs')
+            try
+                dd  = dd(control.dirs);
+                len = length(dd);
+                fprintf(fid, '[%s] - using [%s]', LOCAL_CONTROL, num2str(control.dirs));
+            catch
+                fprintf(fid,'\n[%s] - directory spec error\n', LOCAL_CONTROL);
+                return
+            end
+        else
+            fprintf(fid,'\n(experiment ignored)\n');
+            return
+        end
     end
     % check each directory for .r0 files
+    fprintf(fid,'\n');
     for i = 1:len
         d   = dir(fullfile(dd{i}, R0_SELECT));
         if isempty(d)
@@ -83,6 +118,15 @@ fprintf(fid, 'Post: %s\n', dd{3});
 fprintf(fid, '----\n')
 % run the analysis script, first in the pre/post directories, then in the
 % induction directory
-[pre, pst]   = feval(ANALYSIS_FN_RESP,dd{1},dd{3}, fid);
+if isfield(control,'t_pre') & isfield(control,'t_post')
+    fprintf(fid, '[%s] - using preset timing data\n', LOCAL_CONTROL);
+else
+    control.t_pre   = [];
+    control.t_post  = [];
+end
+[pre,pst]   = feval(ANALYSIS_FN_RESP,dd{1},dd{3}, fid, control.t_pre, control.t_post);
+
 fprintf(fid, '----\n')
-t_post       = feval(ANALYSIS_FN_SPIKE,dd{2}, fid);
+if induced ~= -1
+    t_post       = feval(ANALYSIS_FN_SPIKE,dd{2}, fid);
+end
