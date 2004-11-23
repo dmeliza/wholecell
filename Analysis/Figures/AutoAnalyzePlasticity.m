51bd22ccd93fcd6d38a4b401a3d062f9ae99e290
meliza <>
2004-11-23 22:35:37 +0000

    added a bunch of autoplot stuff. works with test electrical eps

--- Analysis/Figures/AutoAnalyzePlasticity.m
+++ Analysis/Figures/AutoAnalyzePlasticity.m
@@ -36,6 +36,7 @@ ANALYSIS_FN_SPIKE   = 'AutoAnalyzeSpikeTiming';
 DAQ2MAT_MODE    = 'stack';
 DAQ2MAT_CHAN    = 1;
 LOCAL_CONTROL   = 'auto.mat';
+DEBUG           = 1;
 
 % load the control file, if there is one
 if exist(LOCAL_CONTROL) ~= 0
@@ -121,8 +122,8 @@ fprintf(fid, '----\n')
 if isfield(control,'t_pre') & isfield(control,'t_post')
     fprintf(fid, '[%s] - using preset timing data\n', LOCAL_CONTROL);
 else
-    control.t_pre   = [];
-    control.t_post  = [];
+    control(1).t_pre   = [];
+    control(1).t_post  = [];
 end
 [pre,pst]   = feval(ANALYSIS_FN_RESP,dd{1},dd{3}, fid, control.t_pre, control.t_post);
 
@@ -130,3 +131,91 @@ fprintf(fid, '----\n')
 if induced ~= -1
     t_post       = feval(ANALYSIS_FN_SPIKE,dd{2}, fid);
 end
+
+% now we plot and write data to disk, if the correct global variables are
+% set, and if nothing broke during the analysis
+if isempty(pst) | isempty(pre)
+    return
+end
+% generate the figures for each stimulus
+for i = 1:length(pre)
+    fig     = plotdata(pre(i),pst(i),t_post);
+    set(fig,'Name',sprintf('Stimulus %d',i));
+    if WRITE_FIGURES
+        set(fig,'visible','on');
+        fn  = sprintf('resp-%1.0f',i);
+        fn  = fullfile(pwd,[fn '.fig']);
+        saveas(fig,fn,'fig')
+        set(fig,'visible','off')
+    end
+    if ~DEBUG
+        delete(fig)
+    else
+        set(fig,'visible','on')
+    end
+end
+
+function figh   = plotdata(pre, pst, t_post)
+% the summary figure for this plot is going to depend on whether the
+% stimulus was electrical or visual. For electrical stimulation, we'll
+% plot the response, sr, and ir as a function of trace start time.
+BINSIZE = 1;        % bin the IR and SR data to 1 minutes
+figh    = figure;
+set(figh,'visible','off')
+% adjust times
+offset  = etime(pst.start, pre.start)/60;   % time in min between starts
+pst.time= pst.time + offset;
+% some serious subplot-fu here
+ax      = subplot(4,3,[1 2 4 5]);
+plotTimeCourse(ax,pre.time,pre.resp,pst.time,pst.resp);
+set(ax,'XTickLabel',[])
+xlim    = get(ax,'Xlim');
+ylabel(sprintf('Response (%s)',pre.units));
+title('Time Course');
+
+ax      = subplot(4,3,[7 8]);
+plotTimeCourse(ax,pre.time,pre.ir,pst.time,pst.ir,BINSIZE);
+set(ax,'XTickLabel',[],'Xlim',xlim)
+ylabel('IR')
+
+ax      = subplot(4,3,[10 11]);
+plotTimeCourse(ax,pre.time,pre.sr,pst.time,pst.sr,BINSIZE);
+set(ax,'Xlim',xlim)
+ylabel('SR')
+xlabel('Time (min)')
+
+% plot the two average responses and their difference
+ax      = subplot(4,3,3);
+tr1     = pre.trace - mean(pre.trace(1:50));
+tr2     = pst.trace - mean(pst.trace(1:50));
+h       = plot(pre.time_trace,tr1,'k',pst.time_trace,tr2,'r');
+axis tight, set(gca,'XTickLabel',[])
+xlim    = get(gca,'XLim');
+vline(t_post,'k:')
+title('Temporal RF')
+ax      = subplot(4,3,6);
+% this may break...
+h       = plot(pre.time_trace,tr1 - tr2,'k');
+set(gca,'Xlim',xlim)
+vline(t_post,'k:'),hline(0)
+xlabel('Time (s)')
+
+
+function [] = plotTimeCourse(ax, pre_time, pre_data, pst_time, pst_data, binsize)
+axes(ax),cla,hold on
+if nargin > 5
+    [t,d,n,st]  = TimeBin(pre_time,pre_data,binsize);
+    mn          = nanmean(d);
+    h           = errorbar(t,d,st./sqrt(n),'k.');
+    [t,d,n,st]  = TimeBin(pst_time,pst_data,binsize);
+    h           = errorbar(t,d,st./sqrt(n),'k.');
+    h           = hline([mn * 1.3, mn * .7]);
+else
+    h       = plot(pre_time,pre_data,'k.');
+    set(h,'markersize',6)
+    mn      = nanmean(pre_data);
+    h       = plot(pst_time,pst_data,'k.');
+    set(h,'markersize',6)
+    h       = hline(mn);
+end
+
