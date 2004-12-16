ab506c82df47bd17ac17e40426a6085fa26b246a
meliza <>
2004-12-16 04:06:28 +0000

    check significance of IR and SR shifts

--- Analysis/Figures/AutoSelect.m
+++ Analysis/Figures/AutoSelect.m
@@ -6,6 +6,16 @@ function [Z] = AutoSelect(matfile, output)
 % elements of the structure and return the pre/post plasticity of those
 % cells that meet the criteria.
 %
+% As of 1.8, the IR and SR criteria have been changed to check if the
+% postinduction value is significantly (p < 0.05) greater than 30% or less
+% than -30% (depending on the size of the shift).  A Wilcoxon ranksum
+% nonparametric test is used here to avoid problems with the wacky
+% distributions that we get for these parameters.
+%
+% Also note that to avoid dividing by the small IR and SR current steps, we
+% directly compare the current, but approximate the upper bound as 1/0.7
+% and the lower bound as 1/1.3.
+%
 % $Id$
 
 % here are the criteria.  If empty, the criterion is ignored.
@@ -77,7 +87,7 @@ for i = 1:length(sel)
         continue
     end        
     % baseline length:
-    if ~isempty(BASELINE_LENGTH)
+    if ~isempty(BASELINE_LENGTH) & ~Z(i).skip_time
         baseline_length = pre.time(end) - pre.time(1);
         if baseline_length < BASELINE_LENGTH
             fprintf('%s/%s - rejected; baseline too short (%3.2f)\n',...
@@ -100,7 +110,7 @@ for i = 1:length(sel)
     % baseline slope:
     value           = mean(pre.(RESPONSE_TYPE));
     [slope, stats]  = polyfit(pre.time, pre.(RESPONSE_TYPE), 1);
-    if ~isempty(BASELINE_SLOPE)
+    if ~isempty(BASELINE_SLOPE) & ~Z(i).skip_slope
         if abs(slope(1)) > (value .* BASELINE_SLOPE)
             fprintf('%s/%s - rejected; baseline unstable (%3.2f mean, %3.2f slope)\n',...
                 Z(i).rat,Z(i).cell, value, slope(1));
@@ -110,55 +120,53 @@ for i = 1:length(sel)
     end
     % IR:
     field   = 'ir';
-    pre_r   = nanmean(cat(1,Z(i).pre.(field)));
-    % only select IR values in post measurement interval
-    pst_r   = nanmean(getFromInterval(Z(i).pst,field,POST_INTERVAL));
-    shift_ir = pst_r / pre_r;        
-    if ~isempty(POST_IR) & ~Z(i).skip_ir
-        thresh   = [1/(1 - POST_IR), 1/(1 + POST_IR)];
-        if shift_ir > thresh(1) | shift_ir < thresh(2)
-            fprintf('%s/%s - rejected; IR bad (%3.2f %s %3.2f; %3.2f%%)\n',...
-                Z(i).rat,Z(i).cell,...
-                pre_r, char(187), pst_r, (shift_ir - 1) * 100);
-            sel(i)  = 0;
-            continue            
-        end
+    pre_r   = cat(1,Z(i).pre.(field));
+    pst_r   = getFromInterval(Z(i).pst,field,POST_INTERVAL);
+    [res, pre_m, pst_m, P] = checkResistance(pre_r, pst_r, POST_IR);
+    Z(i).shift_ir   = pst_m / pre_m;
+    if ~Z(i).skip_ir & res
+        fprintf('%s/%s - rejected; IR bad (%3.2f %s %3.2f; %3.2f%%, P = %3.4f)\n',...
+            Z(i).rat,Z(i).cell,...
+            pre_m, char(187), pst_m, (Z(i).shift_ir - 1) * 100, P);
+        sel(i)  = 0;
+        continue
     end
+
     % SR:
     field   = 'sr';
     pre_r   = nanmean(cat(1,Z(i).pre.(field)));
-    % only select IR values in post measurement interval
     pst_r   = nanmean(getFromInterval(Z(i).pst,field,POST_INTERVAL));
-    shift_sr = pst_r / pre_r;
-    if ~isempty(POST_SR) & ~Z(i).skip_sr
-        thresh   = [1/(1 - POST_SR), 1/(1 + POST_SR)];
-        if shift_sr > thresh(1) | shift_sr < thresh(2)
-            fprintf('%s/%s - rejected; SR bad (%3.2f %s %3.2f; %3.2f%%)\n',...
-                Z(i).rat,Z(i).cell,...
-                pre_r, char(187), pst_r, (shift_sr - 1) * 100);
-            sel(i)  = 0;
-            continue            
-        end
+    [res, pre_m, pst_m, P] = checkResistance(pre_r, pst_r, POST_SR);
+    Z(i).shift_sr = pst_m / pre_m;
+    if ~Z(i).skip_sr & res
+        fprintf('%s/%s - rejected; SR bad (%3.2f %s %3.2f; %3.2f%%, P = %3.4f)\n',...
+            Z(i).rat,Z(i).cell,...
+            pre_m, char(187), pst_m, (Z(i).shift_sr - 1) * 100, P);
+        sel(i)  = 0;
+        continue            
+    end
+    % check that there were spikes
+    if isempty(Z(i).t_spike)
+        fprintf('%s/%s - rejected: no spikes\n',...
+            Z(i).rat, Z(i).cell);
+        sel(i) = 0;
+        continue
     end
     
     % if we've reached this point, the experiment is good, and we can
     % calculate and store some computed results
-    fprintf('%s/%s - cell passed!\n',Z(i).rat,Z(i).cell);
+    fprintf('%s/%s - cell passed ',Z(i).rat,Z(i).cell);
     pre_val = pre.(RESPONSE_TYPE);
     pst_val = getFromInterval(pst,RESPONSE_TYPE,POST_INTERVAL);
-    if ~isempty(pre_val) | ~isempty(pst_val)
-        Z(i).pre_value  = mean(pre_val);
-        Z(i).pst_value  = mean(pst_val);
-        [h,p]           = ttest2(pre_val,pst_val);
-        Z(i).P_shift    = p;
-    end
+    Z(i).pre_value  = mean(pre_val);
+    Z(i).pst_value  = mean(pst_val);
+    [h,p]           = ttest2(pre_val,pst_val);
+    Z(i).P_shift    = p;
     Z(i).pre_slope  = slope(1);
-    Z(i).shift_ir   = shift_ir;
-    Z(i).shift_sr   = shift_sr;
-    if ~isempty(Z(i).t_spike)
-        Z(i).spike_peak = Z(i).t_spike - pre.t_peak;
-        Z(i).spike_onset = Z(i).t_spike - pre.t_onset;
-    end
+    Z(i).spike_peak = Z(i).t_spike - pre.t_peak;
+    Z(i).spike_onset = Z(i).t_spike - pre.t_onset;
+
+    fprintf('(%3.3f, dt = %3.2f)\n', Z(i).pst_value / Z(i).pre_value, Z(i).spike_peak)
     % also, for futher analysis we're only going to keep the induced bars
     Z(i).pre    = pre;
     Z(i).pst    = pst;
@@ -183,6 +191,33 @@ if nargin > 1
     catch
         fclose(fid);
         error(lasterr)
+    end
+end
+
+function [res, pre_m, pst_m, P] = checkResistance(pre_val, pst_val, THRESH)
+% compares two resistance measurements to determine if they have
+% SIGNIFICANTLY crossed the threshhold. Returns the means.
+ALPHA   = 0.05;
+res     = 0;
+P       = 1;
+% remove NaN's:
+pre_val = pre_val(~isnan(pre_val));
+pst_val = pst_val(~isnan(pst_val));
+% compute the mean shift
+pre_m    = mean(pre_val);
+pst_m    = mean(pst_val);
+if ~isempty(THRESH)
+    shift_ir = pst_m / pre_m;
+    thresh   = [1/(1 - THRESH), 1/(1 + THRESH)];
+    if shift_ir > thresh(1)
+        % check for significantly greater than thresh
+        P   = ranksum(pre_val .* thresh(1), pst_val);
+    elseif shift_ir < thresh(2)
+        % check for significantly less than
+        P   = ranksum(pre_val .* thresh(2), pst_val);
+    end
+    if P < ALPHA
+        res = 1;
     end
 end
 
