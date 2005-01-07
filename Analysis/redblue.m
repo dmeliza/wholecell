e9721571dbdd19484ac8b31febc9a84da582ddc4
meliza <>
2005-01-07 19:25:16 +0000

    can change centerpoint of colormap

--- Analysis/redblue.m
+++ Analysis/redblue.m
@@ -1,9 +1,14 @@
-function h = redblue(alpha,m)
+function h = redblue(alpha,m, mu)
 %REDBLUE    Red-white-blue color map.
-%   REDBLUE(ALPHA,M) returns an M-by-3 matrix containing a "redblue" colormap.
-%            The paramter ALPHA controls the amount of the map that is not white
+%   REDBLUE(ALPHA,M,[MU]) returns an M-by-3 matrix containing a "redblue" colormap.
+%            The paramter ALPHA controls the amount of the map that is not
+%            white.  A value of 1 leaves no white.
+%            MU is used when the zero point of the colormap is not in 
+%            the precise middle. It should reside in [-1 1], and the
+%            default is 0
+%
 %   REDBLUE(ALPHA) is the same length as the current colormap.
-%   REDBLUE, by itself, has a default alpha of 1 (3/8 of colormap is white)
+%   REDBLUE, by itself, has a default alpha of .187 (3/8 of colormap is white)
 %
 %   For example, to reset the colormap of the current figure:
 %
@@ -16,11 +21,23 @@ function h = redblue(alpha,m)
 %   $Id$
 
 if nargin < 2, m = size(get(gcf,'colormap'),1); end
-if nargin < 1, alpha = 3/8; end
-n = fix(m*alpha);
+if nargin < 1, alpha = 3/16; end
+if nargin < 3 
+    mu = 0;
+end
 
-r = [(1:n)'/n; ones(m-n,1)];
-b = [ones(m-n,1); flipud((1:n)')/n];
+% convert parameters to useful indices
+MU = fix(mu * m/2 + m/2);       % if mu is zero, MU is in the center of the map
+n  = fix(m*alpha/2);            % this is the number of points to leave white
+
+if length(n)==1
+    n   = [n n];
+end
+nr = MU - n(1);
+nb = MU + n(2);
+
+r = [(1:nr)'/nr; ones(m-nr,1)];
+b = [ones(nb,1); flipud((1:(m-nb))')/(m-nb)];
 g = r + b - 1;
 
 h = [r g b];
