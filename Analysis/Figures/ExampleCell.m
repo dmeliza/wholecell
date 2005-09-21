6faa58eab6dcfa72da225cbe922bb516a4895b80
meliza <>
2005-09-21 16:57:17 +0000

    many changes, now generates the plots used in Figure 2 of the neuron paper (minus the induction inset, add this later if feeling perverse)

--- Analysis/Figures/ExampleCell.m
+++ Analysis/Figures/ExampleCell.m
@@ -1,128 +1,111 @@
-function [] = ExampleCell(filename, start_time, end_time)
+function [] = ExampleCell(timecourse, pre, post, x_pos, t_spike)
 %
-% Script to produce a nifty example cell figure
-% Loads a mat file exported from episodeanalysis. Plots each variable in
-% its own axes. Leaves the first variable unbinned but bins the others
-% to one minute.
+% Script to produce a nifty example cell figure, as in figure 2 of the
+% Neuron paper.
 %
-% ExampleCell(matfile, [start_time, [end_time]])
-%
-% specify start_time and end_time to control which points to include
+% EXAMPLECELL(timecourse, pretrace, posttrace, x_pos, [t_spike])
+% TIMECOURSE is a matfile exported from episodeanalysis that has (at least)
+% the time course of the response. PRETRACE and POSTTRACE are also from
+% EpisodeAnalysis, the same files used by CompareRF.  X_POS must be
+% supplied so that the right trace gets plotted.
 
 % $Id$
-BINSIZE = 1;
-V_STEP  = 5; % mV
-I_STEP  = 0.3; % nA
-ZMAX    = 1.5;
-RESPONSE_ONLY = 1;
 
-if nargin < 3
-    end_time = [];
+FIGURE  = [3.25 2.5];
+FIELD   = 'results';
+XLABEL  = 'Time (min)';
+YLABEL  = 'Response Amplitude (%s)';
+START   = 200;          % start time of plot, ms
+WIN     = [-10 510];    % length of plot (rel to START)
+LIGHT   = [0 500];      % time when bar of light is on (rel to START)
+
+error(nargchk(4,5,nargin))
+
+z   = load(timecourse);
+z   = z.results;
+A   = load(pre);
+B   = load(post);
+if size(A.data,2) < x_pos
+    error('Spatial index exceeds number of traces in data')
 end
-if nargin < 2
-    start_time = [];
-end
 
-z = load(filename);
-z = z.results;
-
+% Open the figure
 f = figure;
 set(f,'color',[1 1 1]);
-set(f,'units','inches')
-p   = get(f,'position');
-p   = [p(1) p(2) 3.8 2.2];
-set(f,'position',p);
+ResizeFigure(f,FIGURE)
 
-nplot   = length(z);
-p       = 3;
-% p       = (nplot-1)*2;
-% a       = subplot(p,1,[1:nplot-1]);
-a       = subplot(p,1,[1:2]);
-nplot   = 2;
+% set up the axes. This is one large lower axis for the time course and a
+% smaller axis above it for the visual response
+ax1     = subplot(2,1,2);   % bottom 1/2 of figure
 hold on
 
-% plot the response data first
+% plot the timecourse
 offset = 0;
-for i = 1:length(z(1).results)
-    t   = z(1).results(i).abstime;
-    v   = z(1).results(i).value;
-    if i==1 & ~isempty(start_time)
-        ind = find(t>=start_time);
-        offset = t(ind(1));
-        t   = t(ind);
-        v   = v(ind);
-    elseif i==length(z(1).results) & ~isempty(end_time)
-        ind = find(t<=end_time);
-        t   = t(ind);
-        v   = v(ind);
-    end
+for i = 1:length(z(1).(FIELD))
+    t   = z(1).(FIELD)(i).abstime;
+    v   = z(1).(FIELD)(i).value;
     t   = t - offset;
     h   = plot(t(v>0),v(v>0),'k.');
     set(h,'markersize',6)
-    mn = mean(v(v>0));
-    h = plot([t(1) t(end)],[mn mn],'r:');
-    fprintf('Episode %d: %3.2f\n',i,mn);
-    set(h,'linewidth',2)
-end
-if i > 1
-    [H,P]   = ttest2(z(1).results(1).value, z(1).results(2).value);
-    fprintf('p = %4.4f',P);
-end
-
-ylim    = get(gca,'YLim');
-h   = plot(z(1).results(1).abstime(end) + 1, ylim(2) * 0.8,'kv');
-set(h,'MarkerFaceColor',[0 0 0]);
-
-ylabel(sprintf('EPSC Amplitude (%s)',z(1).results(i).units));
-
-if RESPONSE_ONLY
-    xlabel('Time (min)');    
-    return
-else
-    set(a,'XTickLabel',[]);
-end
-    
-% now plot the ancillary data
-for i = 2:2 %length(z)
-    nplot   = nplot+1;
-    a  = subplot(p,1,nplot);
-    hold on
-    for j = 1:length(z(i).results)
-        t               = z(i).results(j).abstime;
-        v               = z(i).results(j).value;
-        t               = t(v>0);
-        v               = v(v>0);
-        [t, v, n, stdv] = TimeBin(t, v, BINSIZE);
-        switch lower(z(i).name)
-            case {'ir', 'sr'}
-                % convert current values into resistance
-                % (V = IR)
-                % note that the division operation can make a mess
-                switch lower(z(i).results(j).units)
-                    case 'pa'
-                        v   = V_STEP ./ v * 1000;
-                    case 'na'                
-                        v  = V_STEP ./ v;
-                    case 'mV'
-                        v  = v ./ I_STEP;
-                end
-                % clean up noise
-                Z   = zscore(v);
-                Z   = abs(Z) <= ZMAX;
-                t   = t(Z);
-                v   = v(Z);
-                z(i).results(j).units = 'M\Omega';  % TeX markup
-        end
-        h   = plot(t,v,'ko');
+    if i == 1
+        mn  = mean(v(v>0)); % used to plot horizontal line
     end
-    set(a,'XTickLabel',[])
-    ylabel(sprintf('%s (%s)', z(i).name, z(i).results(j).units));
+    fprintf('%d traces\n',length(t));
 end
-set(a,'XTickLabelMode','Auto')
-xlabel('Time (min)')
-ax      = findobj(f,'type','axes');
-xt      = get(ax,'XTick');
-len     = cellfun('length',xt);
-[m,i]   = min(len);
-xl      = get(ax(i),'XLim');
-set(ax,'XLim',xl);
+% Make the plot pretty
+h   = hline(mn,'k:');
+set(h,'LineWidth',2)
+% the user will have to adjust the Xlim by hand
+ylim    = get(gca,'YLim');
+h   = plot(z(1).results(1).abstime(end) + 2, ylim(2) * 0.8,'kv');
+set(h,'MarkerFaceColor',[0 0 0]);
+xlabel(XLABEL)
+ylabel(sprintf(YLABEL,z(1).(FIELD)(i).units))
+
+% Inset: right 2/3 of top 1/2 of figure
+ax2 = subplot(2,3,[2 3]);
+% Synchronize time variables
+A.time  = double(A.time) * 1000 - START;
+B.time  = double(B.time) * 1000 - START;
+a_ind   = find(A.time >= WIN(1) & A.time <= WIN(2));
+b_ind   = find(B.time >= WIN(1) & B.time <= WIN(2));
+if length(a_ind) > length(b_ind)
+    a_ind = a_ind(1:length(b_ind));
+else
+    b_ind = b_ind(1:length(a_ind));
+end
+% Get the traces
+t       = A.time(a_ind);
+a       = A.data(a_ind, x_pos);
+b       = B.data(b_ind, x_pos);
+% Remove DC offset
+a       = a - mean(a(1:50));
+b       = b - mean(b(1:50));
+% Plot
+h       = plot(t,a,'k',t,b,'r');
+% set(h,'LineWidth',2);
+mx  = max(max([a b]));
+mn  = min(min([a b]));
+set(gca,'YLim',[mn * 1.1, mx * 1.1]);
+% Draw spike time, if supplied
+if nargin > 4
+    vline(t_spike,'k-')     % the line style has to be adjusted in illustrator
+end
+axis tight
+% Add the stimulus bar
+hold on
+y   = mx * 1.6;
+h   = plot(LIGHT,[y y]);
+set(h,'color','black','linewidth',2)
+set(gca,'ylim',[mn * 1.1, mx * 1.7])
+
+% Add the scale bar
+xtick   = 100;
+ytick   = 0;    % automatic
+AddScaleBar(gca,{'ms','pA'}, [xtick ytick]);
+
+% finally, adjust the size of the axes
+% (this code is hardwired because I am lazy, but it should be in relative
+% coordinates)
+set(ax1,'Position',[0.1397    0.1583    0.7653    0.4042])
+set(ax2,'Position',[0.5184    0.6000    0.3900    0.3250]);
